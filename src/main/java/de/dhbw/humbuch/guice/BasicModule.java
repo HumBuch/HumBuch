626d7413daaf3832bed461a24c0c23f413a98f38
davherrmann <davherrmann@googlemail.com>
2013-12-11 17:57:22 +0100

    implemented functional DAO injection, DAO example use switched back to deploy2jetty due to jetty6 errors with hibernate

--- src/main/java/de/dhbw/humbuch/guice/BasicModule.java
+++ src/main/java/de/dhbw/humbuch/guice/BasicModule.java
@@ -1,12 +1,17 @@
 package de.dhbw.humbuch.guice;
 
 import com.google.inject.Provides;
+import com.google.inject.TypeLiteral;
 import com.google.inject.multibindings.MapBinder;
+import com.google.inject.persist.jpa.JpaPersistModule;
 import com.google.inject.servlet.ServletModule;
 import com.vaadin.ui.UI;
 
 import de.davherrmann.guice.vaadin.UIScoped;
 import de.davherrmann.mvvm.ViewModelComposer;
+import de.dhbw.humbuch.model.DAO;
+import de.dhbw.humbuch.model.DAOImpl;
+import de.dhbw.humbuch.model.entity.Student;
 import de.dhbw.humbuch.view.BasicUI;
 import de.dhbw.humbuch.view.LoginView;
 import de.dhbw.humbuch.view.MVVMConfig;
@@ -16,7 +21,12 @@ public class BasicModule extends ServletModule {
 
 	@Override
 	protected void configureServlets() {
+		install(new JpaPersistModule("persistModule"));
+		
+		filter("/*").through(PersistFilter.class);
 		serve("/*").with(BasicServlet.class);
+		
+		bind(new TypeLiteral<DAO<Student>>() {}).to(new TypeLiteral<DAOImpl<Student>>() {});
 		
 		bind(ViewModelComposer.class).asEagerSingleton();
 		bind(MVVMConfig.class).asEagerSingleton();
