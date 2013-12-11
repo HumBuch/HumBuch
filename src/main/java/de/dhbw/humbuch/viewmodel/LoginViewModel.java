c3e5abd6d99d67b151685767c117f5ff5c316fa6
davherrmann <davherrmann@googlemail.com>
2013-12-11 10:59:19 +0100

    added DAO injecting example

--- src/main/java/de/dhbw/humbuch/viewmodel/LoginViewModel.java
+++ src/main/java/de/dhbw/humbuch/viewmodel/LoginViewModel.java
@@ -1,16 +1,23 @@
 package de.dhbw.humbuch.viewmodel;
 
+import com.google.inject.Inject;
+
 import de.davherrmann.mvvm.ActionHandler;
 import de.davherrmann.mvvm.BasicState;
 import de.davherrmann.mvvm.State;
 import de.davherrmann.mvvm.annotations.HandlesAction;
 import de.davherrmann.mvvm.annotations.ProvidesState;
+import de.dhbw.humbuch.model.DAO;
+import de.dhbw.humbuch.model.entity.Student;
 
 public class LoginViewModel {
 
 	public interface LoginSuccessful extends State<Boolean> {}
 
 	public interface DoLogin extends ActionHandler {}
+	
+	@Inject
+	private DAO<Student> daoStudent;
 
 	@ProvidesState(LoginSuccessful.class)
 	public final BasicState<String> loginSuccessful = new BasicState<>(String.class);
