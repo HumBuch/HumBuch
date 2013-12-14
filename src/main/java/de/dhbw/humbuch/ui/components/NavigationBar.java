318f869753fc882927711049d6998eb7c59447d9
jidel <Johannes@172.16.52.228>
2013-12-14 17:05:54 +0100

    Added first version of a multi-class-loader and edited NavigationBar

--- src/main/java/de/dhbw/humbuch/ui/components/NavigationBar.java
+++ src/main/java/de/dhbw/humbuch/ui/components/NavigationBar.java
@@ -1,38 +1,31 @@
 package de.dhbw.humbuch.ui.components;
 
+
 import com.vaadin.annotations.Theme;
 import com.vaadin.ui.Alignment;
-import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.CustomComponent;
+import com.vaadin.ui.NativeButton;
 import com.vaadin.ui.VerticalLayout;
-
-import de.dhbw.humbuch.ui.NavigationUI;
-
 
 @Theme("mytheme")
 public class NavigationBar extends CustomComponent /*implements IComponent*/ {
 
-	private static final String NAV_HOME = "Aufgaben";
-	private static final String NAV_BORROW = "Ausleihe";
-	private static final String NAV_RETURN = "Rueckgabe";
-	private static final String NAV_MAINTAIN_CLASS = "Klasse einpflegen";
-	private static final String NAV_MAINTAIN_PUPIL = "Schueler einpflegen";
-	private static final String NAV_INFO_LIST = "Auskunftslisten";
-	private static final String NAV_DUNNING = "Mahnverfahren";
-	private static final String NAV_MANAGE_BOOKS = "Buecher Verwaltung";
-	private static final String NAV_IMPORT = "Import";
-
+	/**
+	 * Each navigation button has to be added to the array "navigationButtons".
+	 * First value of the array is the human readable caption of the button, the second
+	 * one the name of the specific view to load.
+	 */
+	private static final String[][] navigationButtons = new String[][] {
+		{"Aufgaben", "main"},
+		{"Ausleihe", "manageBorrows"},
+		{"R\u00FCckgabe", "manageReturns"},
+		{"Sch\u00FCler", "managePupils"},
+		{"B\u00FCcher", "manageBooks"},
+		{"Mahnungen", "managedunnings"}
+	};
+	
 	private VerticalLayout verticalLayoutNavBar;
-	private Button buttonHome;
-	private Button buttonBorrow;
-	private Button buttonReturn;
-	private Button buttonMaintainClass;
-	private Button buttonMaintainPupil;
-	private Button buttonInfoList;
-	private Button buttonDunning;
-	private Button buttonManageBooks;
-	private Button buttonImport;
 
 	public NavigationBar() {
 
@@ -40,102 +33,26 @@ public class NavigationBar extends CustomComponent /*implements IComponent*/ {
 		 * 
 		 */
 		verticalLayoutNavBar = new VerticalLayout();
-		buttonHome = new Button(NAV_HOME, new Button.ClickListener() {
-			@Override
-			public void buttonClick(ClickEvent event) {
-				System.out.println(getUI());
-				getUI().getNavigator().navigateTo(NavigationUI.MAINVIEW);
-			}
-		});
-		buttonBorrow = new Button(NAV_BORROW, new Button.ClickListener() {
-			@Override
-			public void buttonClick(ClickEvent event) {
-				System.out.println(getUI());
-				getUI().getNavigator().navigateTo(NavigationUI.MANAGE_BOOKS);
-			}
-		});
-		buttonReturn = new Button(NAV_RETURN, new Button.ClickListener() {
-			@Override
-			public void buttonClick(ClickEvent event) {
-				System.out.println(getUI());
-				getUI().getNavigator().navigateTo(NavigationUI.MANAGE_BOOKS);
-			}
-		});
-		buttonMaintainClass = new Button(NAV_MAINTAIN_CLASS, new Button.ClickListener() {
-			@Override
-			public void buttonClick(ClickEvent event) {
-				System.out.println(getUI());
-				getUI().getNavigator().navigateTo(NavigationUI.MANAGE_BOOKS);
-			}
-		});
-		buttonMaintainPupil = new Button(NAV_MAINTAIN_PUPIL, new Button.ClickListener() {
-			@Override
-			public void buttonClick(ClickEvent event) {
-				System.out.println(getUI());
-				getUI().getNavigator().navigateTo(NavigationUI.MANAGE_BOOKS);
-			}
-		});
-		buttonInfoList = new Button(NAV_INFO_LIST, new Button.ClickListener() {
-			@Override
-			public void buttonClick(ClickEvent event) {
-				System.out.println(getUI());
-				getUI().getNavigator().navigateTo(NavigationUI.MANAGE_BOOKS);
-			}
-		});
-		buttonDunning = new Button(NAV_DUNNING, new Button.ClickListener() {
-			@Override
-			public void buttonClick(ClickEvent event) {
-				System.out.println(getUI());
-				getUI().getNavigator().navigateTo(NavigationUI.DUNNING);
-			}
-		});
-		buttonManageBooks = new Button(NAV_MANAGE_BOOKS, new Button.ClickListener() {
-			@Override
-			public void buttonClick(ClickEvent event) {
-				System.out.println(getUI());
-				getUI().getNavigator().navigateTo(NavigationUI.MANAGE_BOOKS);
-			}
-		});
-		buttonImport = new Button(NAV_IMPORT, new Button.ClickListener() {
-			@Override
-			public void buttonClick(ClickEvent event) {
-				System.out.println(getUI());
-				getUI().getNavigator().navigateTo(NavigationUI.MANAGE_BOOKS);
-			}
-		});
-
 		
-		String width = "100%";
-		buttonHome.setWidth(width);
-		buttonBorrow.setWidth(width);
-		buttonReturn.setWidth(width);
-		buttonMaintainClass.setWidth(width);
-		buttonMaintainPupil.setWidth(width);
-		buttonInfoList.setWidth(width);
-		buttonDunning.setWidth(width);
-		buttonManageBooks.setWidth(width);
-		buttonImport.setWidth(width);
+		
+		for (final String[] view : navigationButtons) {
+			NativeButton b = new NativeButton(view[0], new NativeButton.ClickListener() {
+				@Override
+				public void buttonClick(ClickEvent event) {
+					System.out.println(getUI());
+					event.getButton().addStyleName("selected");
 
-		verticalLayoutNavBar.setSpacing(true);
-		verticalLayoutNavBar.setMargin(true);
-		verticalLayoutNavBar.addComponent(buttonHome);
-		verticalLayoutNavBar.setComponentAlignment(buttonHome, Alignment.MIDDLE_CENTER);
-		verticalLayoutNavBar.addComponent(buttonBorrow);
-		verticalLayoutNavBar.setComponentAlignment(buttonBorrow, Alignment.MIDDLE_CENTER);
-		verticalLayoutNavBar.addComponent(buttonReturn);
-		verticalLayoutNavBar.setComponentAlignment(buttonReturn, Alignment.MIDDLE_CENTER);
-		verticalLayoutNavBar.addComponent(buttonMaintainClass);
-		verticalLayoutNavBar.setComponentAlignment(buttonMaintainClass, Alignment.MIDDLE_CENTER);
-		verticalLayoutNavBar.addComponent(buttonMaintainPupil);
-		verticalLayoutNavBar.setComponentAlignment(buttonMaintainPupil, Alignment.MIDDLE_CENTER);
-		verticalLayoutNavBar.addComponent(buttonInfoList);
-		verticalLayoutNavBar.setComponentAlignment(buttonInfoList, Alignment.MIDDLE_CENTER);
-		verticalLayoutNavBar.addComponent(buttonDunning);
-		verticalLayoutNavBar.setComponentAlignment(buttonDunning, Alignment.MIDDLE_CENTER);
-		verticalLayoutNavBar.addComponent(buttonManageBooks);
-		verticalLayoutNavBar.setComponentAlignment(buttonManageBooks, Alignment.MIDDLE_CENTER);
-		verticalLayoutNavBar.addComponent(buttonImport);
-		verticalLayoutNavBar.setComponentAlignment(buttonImport, Alignment.MIDDLE_CENTER);
+					if(!getUI().getNavigator().getState().equals(view[1])) {
+						getUI().getNavigator().navigateTo(view[1]);
+					}
+				}
+			});
+			b.addStyleName("icon-" + view);
+			b.setWidth("100%");
+			verticalLayoutNavBar.addComponent(b);
+			verticalLayoutNavBar.setComponentAlignment(b, Alignment.MIDDLE_CENTER);
+			
+		}
 		
 		setCompositionRoot(verticalLayoutNavBar);
 	}
