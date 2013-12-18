7c0d105d3b1dda370874e9629c45a0a510d5132f
BennyRa <bennyra92@gmail.com>
2013-12-18 09:32:24 +0100

    - changed student creation in csvImport to student builder

--- src/main/java/de/dhbw/humbuch/dataImport/MyCSVHandler.java
+++ src/main/java/de/dhbw/humbuch/dataImport/MyCSVHandler.java
@@ -2,14 +2,19 @@ package de.dhbw.humbuch.dataImport;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Locale;
 import java.util.Set;
 
 import au.com.bytecode.opencsv.CSVReader;
 import de.dhbw.humbuch.model.ProfileTypeHandler;
 import de.dhbw.humbuch.model.StudentHandler;
+import de.dhbw.humbuch.model.entity.Grade;
 import de.dhbw.humbuch.model.entity.ProfileType;
 import de.dhbw.humbuch.model.entity.Student;
 
@@ -62,7 +67,21 @@ public final class MyCSVHandler {
 				
 		//Profile profile = ProfileHandler.createProfile(record[0], record[1], record[2]);
 		//return StudentHandler.createStudentObject(record[16], record[9], record[5], record[6], record[8], profileTypeSet);
-		Date date = new SimpleDateFormat("MMMM d, yyyy", Locale.ENGLISH).parse(string);
-		return new Student.Builder(record[16],record[9],  record[5], record[8]).build();
+		Date date = null;
+		try {
+			date = new SimpleDateFormat("dd.mm.yyyy", Locale.GERMAN).parse(record[5]);
+		}
+		catch (ParseException e) {
+			System.err.println("Could not format date " + e.getStackTrace());
+		}
+		Grade grade = new Grade();
+		String[] splittedString = splitBetweenCharsAndDigits(record[8]);
+		grade.setGrade(Integer.parseInt(splittedString[0]));
+		grade.setSuffix(splittedString[1]);
+		return new Student.Builder(0, record[16], record[9],  date, grade).profileTypes(profileTypeSet).gender(record[6]).build();
+	}
+	
+	private static String[] splitBetweenCharsAndDigits(String str){	
+		return str.split("(?<=\\D)(?=\\d)|(?<=\\d)(?=\\D)");
 	}
 }
