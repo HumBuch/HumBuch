698f3fb6f9712597bdd55ba3decb25ebc284879f
BennyRa <bennyra92@gmail.com>
2013-12-18 09:40:49 +0100

    - removed StudentHandler.createStudentObject

--- src/main/java/de/dhbw/humbuch/model/StudentHandler.java
+++ src/main/java/de/dhbw/humbuch/model/StudentHandler.java
@@ -15,29 +15,4 @@ public class StudentHandler {
 	public static String getFullNameOfStudent(Student student){
 		return student.getFirstname() + " " + student.getLastname();
 	}
-	
-	public static Student createStudentObject(String firstName, String lastName, String birthDayString, 
-												String gender, String gradeString, Set<ProfileType> profileTypeSet){
-		Student student = new Student();
-		try {
-			student.setFirstname(firstName);
-			student.setLastname(lastName);		
-			student.setBirthday(new SimpleDateFormat("dd.MM.yyyy").parse(birthDayString));
-			student.setGender(gender);
-			Grade grade = new Grade();
-			String[] splittedString = splitBetweenCharsAndDigits(gradeString);
-			grade.setGrade(Integer.parseInt(splittedString[0]));
-			grade.setSuffix(splittedString[1]);
-			student.setGrade(grade);
-			student.setProfileTypes(profileTypeSet);
-		}catch (ParseException e) {
-			System.err.println("Could not parse String to Date " + e.getMessage());
-		}
-		
-		return student;
-	}
-	
-	private static String[] splitBetweenCharsAndDigits(String str){	
-		return str.split("(?<=\\D)(?=\\d)|(?<=\\d)(?=\\D)");
-	}
 }
