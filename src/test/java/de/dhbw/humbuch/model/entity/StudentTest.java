85d12e8271c1dc2ca1bf4f8c9306b3443c549481
BennyRa <bennyra92@gmail.com>
2013-12-18 09:33:39 +0100

    - fixed student test -> added id to builder.call

--- src/test/java/de/dhbw/humbuch/model/entity/StudentTest.java
+++ src/test/java/de/dhbw/humbuch/model/entity/StudentTest.java
@@ -10,12 +10,14 @@ public class StudentTest {
 	
 	@Test
 	public void testStudentBuilder() {
+		final int ID = 4;
 		final String FIRST_NAME = "John";
 		final String LAST_NAME = "Doe";
 		final Date DATE = new Date();
 		final Grade GRADE = null;
 		
-		Student student = new Student.Builder(FIRST_NAME, LAST_NAME, DATE, GRADE).build();
+		Student student = new Student.Builder(ID, FIRST_NAME, LAST_NAME, DATE, GRADE).build();
+		assertEquals(ID, student.getId());
 		assertEquals(FIRST_NAME, student.getFirstname());
 		assertEquals(LAST_NAME, student.getLastname());
 		assertEquals(DATE, student.getBirthday());
