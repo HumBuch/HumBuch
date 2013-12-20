2efda438e5ff0216c53fc1c78fa1a818b09f6b73
Henning Muszynski <henningmuszynski@web.de>
2013-12-20 14:34:47 +0100

    Switched from VerticalLayout to Panel. Now scrolling is available

--- src/main/java/de/dhbw/humbuch/view/DunningView.java
+++ src/main/java/de/dhbw/humbuch/view/DunningView.java
@@ -7,6 +7,7 @@ import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Panel;
 import com.vaadin.ui.Table;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
@@ -14,10 +15,12 @@ import com.vaadin.ui.VerticalLayout;
 import de.davherrmann.mvvm.ViewModelComposer;
 import de.dhbw.humbuch.viewmodel.DunningViewModel;
 
-public class DunningView extends VerticalLayout implements View {
+
+public class DunningView extends Panel implements View {
 
 	private static final long serialVersionUID = 1284094636968999625L;
 
+	private static final String TITLE = "Mahnungs Übersicht";
 	private static final String SECOND_DUNNING = "2. Mahnung";
 	private static final String RETURN_BOOK = "Buch zurueck";
 	private static final String NEW_DUNNING = "Neue Mahnung";
@@ -30,6 +33,7 @@ public class DunningView extends VerticalLayout implements View {
 	private static final String TABLE_CLASS = "Klasse";
 	private static final String TABLE_TYPE = "Typ";
 
+	private VerticalLayout verticalLayoutContent;
 	private HorizontalLayout horizontalLayoutButtonBar;
 	private Button buttonSecondDunning;
 	private Button buttonReturnBook;
@@ -48,6 +52,7 @@ public class DunningView extends VerticalLayout implements View {
 	}
 
 	private void init() {
+		verticalLayoutContent = new VerticalLayout();
 		horizontalLayoutButtonBar = new HorizontalLayout();
 		buttonSecondDunning = new Button(SECOND_DUNNING);
 		buttonReturnBook = new Button(RETURN_BOOK);
@@ -75,45 +80,49 @@ public class DunningView extends VerticalLayout implements View {
 		tableSearchResults
 				.addContainerProperty(TABLE_CLASS, String.class, null);
 
+		verticalLayoutContent.setSpacing(true);
+		verticalLayoutContent.setMargin(true);
+
 		horizontalLayoutButtonBar.setSpacing(true);
+
+		setSizeFull();
+		setCaption(TITLE);
 	}
 
 	private void buildLayout() {
-		setSizeFull();
-		setMargin(true);
-		setSpacing(true);
-		
 		horizontalLayoutButtonBar.addComponent(buttonNewDunning);
 		horizontalLayoutButtonBar.addComponent(buttonSecondDunning);
 		horizontalLayoutButtonBar.addComponent(buttonReturnBook);
-		addComponent(tableDunnings);
-		addComponent(horizontalLayoutButtonBar);
-		addComponent(textFieldSearch);
-		addComponent(tableSearchResults);
-		addComponent(buttonCreateDunning);
+		verticalLayoutContent.addComponent(tableDunnings);
+		verticalLayoutContent.addComponent(horizontalLayoutButtonBar);
+		verticalLayoutContent.addComponent(textFieldSearch);
+		verticalLayoutContent.addComponent(tableSearchResults);
+		verticalLayoutContent.addComponent(buttonCreateDunning);
+
+		setContent(verticalLayoutContent);
 	}
 
 	private void fillTableDunnings() {
 		tableDunnings.addItem(new Object[] { "Hans", "Wurst", "5a",
-				"1. Mahnung" }, 1);
+											"1. Mahnung" }, 1);
 		tableDunnings.addItem(new Object[] { "Peter", "Lustig", "7b",
-				"2. Mahnung" }, 2);
+											"2. Mahnung" }, 2);
 		tableDunnings.addItem(new Object[] { "Angela", "Merkel", "6c",
-				"1. Mahnung generieren" }, 3);
+											"1. Mahnung generieren" }, 3);
 		tableDunnings.addItem(new Object[] { "Max", "Muster", "7a",
-				"1. Mahnung" }, 4);
+											"1. Mahnung" }, 4);
 		tableDunnings.addItem(new Object[] { "Super", "Richie", "6b",
-				"2. Mahnung generieren" }, 5);
+											"2. Mahnung generieren" }, 5);
 		tableDunnings.addItem(new Object[] { "Hannah", "Montana", "5a",
-				"1. Mahnung" }, 6);
+											"1. Mahnung" }, 6);
 		tableDunnings.addItem(new Object[] { "Joko", "Winterscheidt", "8a",
-				"2. Mahnung" }, 7);
+											"2. Mahnung" }, 7);
 		tableDunnings.addItem(new Object[] { "Test", "Name", "5a",
-				"1. Mahnung generieren" }, 8);
+											"1. Mahnung generieren" }, 8);
 		tableDunnings.addItem(new Object[] { "Er mag", "Zuege", "7a",
-				"2. Mahnung" }, 9);
+											"2. Mahnung" }, 9);
 		tableDunnings.addItem(new Object[] { "Heino", "Kein plan", "8c",
-				"2. Mahnung generieren" }, 10);
+											"2. Mahnung generieren" }, 10);
 	}
 
 	@Override
@@ -126,7 +135,8 @@ public class DunningView extends VerticalLayout implements View {
 			Object... viewModels) {
 		try {
 			viewModelComposer.bind(this, viewModels);
-		} catch (IllegalAccessException | NoSuchElementException
+		}
+		catch (IllegalAccessException | NoSuchElementException
 				| UnsupportedOperationException e) {
 			e.printStackTrace();
 		}
--- src/main/java/de/dhbw/humbuch/view/ReturnView.java
+++ src/main/java/de/dhbw/humbuch/view/ReturnView.java
@@ -7,6 +7,7 @@ import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Panel;
 import com.vaadin.ui.PopupView;
 import com.vaadin.ui.Table;
 import com.vaadin.ui.TextField;
@@ -16,10 +17,12 @@ import de.davherrmann.mvvm.ViewModelComposer;
 import de.dhbw.humbuch.view.components.MultiClassChooser;
 import de.dhbw.humbuch.viewmodel.ReturnViewModel;
 
-public class ReturnView extends VerticalLayout implements View {
+
+public class ReturnView extends Panel implements View {
 
 	private static final long serialVersionUID = -525078997965992622L;
-	
+
+	private static final String TITLE = "Rückgabe";
 	private static final String BUTTON_MATERIAL = "Materialliste";
 	private static final String BUTTON_LENDING = "Ausleihliste";
 	private static final String INPUT_PROMPT = "Schüler suchen";
@@ -27,12 +30,12 @@ public class ReturnView extends VerticalLayout implements View {
 	private static final String FIRST_NAME = "Vorname";
 	private static final String LAST_NAME = "Nachname";
 	private static final String CLASS = "Klasse";
-	
+
 	private VerticalLayout verticalLayoutContent;
 	private HorizontalLayout horizontalLayoutPopup;
 	private VerticalLayout verticalLayoutPopupFirstColumn;
 	private VerticalLayout verticalLayoutPopupSecondColumn;
-	private MultiClassChooser  classChooser;
+	private MultiClassChooser classChooser;
 	private Button buttonMaterialList;
 	private Button buttonLendingList;
 	private TextField searchbar;
@@ -46,28 +49,28 @@ public class ReturnView extends VerticalLayout implements View {
 		buildLayout();
 		bindViewModel(viewModelComposer, returnViewModel);
 	}
-	
+
 	private void init() {
 		verticalLayoutContent = new VerticalLayout();
 		verticalLayoutContent.setMargin(true);
 		verticalLayoutContent.setSpacing(true);
-		
+
 		horizontalLayoutPopup = new HorizontalLayout();
 		horizontalLayoutPopup.setWidth("300px");
-		
+
 		verticalLayoutPopupFirstColumn = new VerticalLayout();
 		classChooser = new MultiClassChooser();
-		
+
 		buttonLendingList = new Button(BUTTON_LENDING);
 		buttonMaterialList = new Button(BUTTON_MATERIAL);
-		
+
 		verticalLayoutPopupSecondColumn = new VerticalLayout();
-		
+
 		searchbar = new TextField("");
 		searchbar.setInputPrompt(INPUT_PROMPT);
-		
+
 		buttonMaterialListStudent = new Button(BUTTON_LENDING);
-		
+
 		popupView = new PopupView(CHOOSE_LIST, horizontalLayoutPopup);
 		popupView.setHideOnMouseOut(false);
 
@@ -77,49 +80,52 @@ public class ReturnView extends VerticalLayout implements View {
 		tableStudents.addContainerProperty(CLASS, String.class, null);
 		tableStudents.addContainerProperty("", Button.class, null);
 		tableStudents.addContainerProperty("", Button.class, null);
-		
+
 		populateWithTestData(tableStudents);
+
+		setSizeFull();
+		setCaption(TITLE);
 	}
-	
+
 	private void buildLayout() {
 		verticalLayoutPopupFirstColumn.addComponent(classChooser);
 		verticalLayoutPopupFirstColumn.addComponent(buttonMaterialList);
 		verticalLayoutPopupFirstColumn.addComponent(buttonLendingList);
-		
+
 		horizontalLayoutPopup.addComponent(verticalLayoutPopupFirstColumn);
-		
+
 		verticalLayoutPopupSecondColumn.addComponent(searchbar);
 		verticalLayoutPopupSecondColumn.addComponent(buttonMaterialListStudent);
-		
+
 		horizontalLayoutPopup.addComponent(verticalLayoutPopupSecondColumn);
-		
+
 		verticalLayoutContent.addComponent(popupView);
-		
+
 		verticalLayoutContent.addComponent(tableStudents);
-		
-		addComponent(verticalLayoutContent);
+
+		setContent(verticalLayoutContent);
 	}
-	
 
 	private void populateWithTestData(Table tableStudents) {
 		Button dataOk = new Button("Ok");
 		Button dataInvalid = new Button("Editieren");
-		tableStudents.addItem(new Object[] {"5a", "Mustermann", "Max", dataOk, dataInvalid}, 1);
-		tableStudents.addItem(new Object[] {"8b", "Maier", "Clara", dataOk, dataInvalid}, 2);
-		tableStudents.addItem(new Object[] {"9c", "Mustermann", "Hans", dataOk, dataInvalid}, 3);
-		tableStudents.addItem(new Object[] {"7a", "XYZ", "BLaa", dataOk, dataInvalid}, 4);
+		tableStudents.addItem(new Object[] { "5a", "Mustermann", "Max", dataOk, dataInvalid }, 1);
+		tableStudents.addItem(new Object[] { "8b", "Maier", "Clara", dataOk, dataInvalid }, 2);
+		tableStudents.addItem(new Object[] { "9c", "Mustermann", "Hans", dataOk, dataInvalid }, 3);
+		tableStudents.addItem(new Object[] { "7a", "XYZ", "BLaa", dataOk, dataInvalid }, 4);
 	}
-	
+
 	private void bindViewModel(ViewModelComposer viewModelComposer,
 			Object... viewModels) {
 		try {
 			viewModelComposer.bind(this, viewModels);
-		} catch (IllegalAccessException | NoSuchElementException
+		}
+		catch (IllegalAccessException | NoSuchElementException
 				| UnsupportedOperationException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	@Override
 	public void enter(ViewChangeEvent event) {
 		// TODO Auto-generated method stub
