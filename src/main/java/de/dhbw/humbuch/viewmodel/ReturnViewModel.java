0dc66adf43d1733fac3a1c4d23512a99d22efe48
davvitt <vitt.da@gmail.com>
2013-12-25 13:04:26 +0100

    little bugfix

--- src/main/java/de/dhbw/humbuch/viewmodel/ReturnViewModel.java
+++ src/main/java/de/dhbw/humbuch/viewmodel/ReturnViewModel.java
@@ -6,14 +6,22 @@ import java.util.List;
 import com.google.inject.Inject;
 
 import de.davherrmann.mvvm.ActionHandler;
+import de.davherrmann.mvvm.BasicState;
+import de.davherrmann.mvvm.State;
 import de.davherrmann.mvvm.annotations.HandlesAction;
+import de.davherrmann.mvvm.annotations.ProvidesState;
 import de.dhbw.humbuch.model.DAO;
 import de.dhbw.humbuch.model.entity.BorrowedMaterial;
 import de.dhbw.humbuch.model.entity.Student;
 
 public class ReturnViewModel {
 	
-	public interface getReturnList extends ActionHandler {};
+	public interface GenerateReturnList extends ActionHandler {};
+	
+	public interface ReturnListStudent extends State<List<BorrowedMaterial>> {};
+	
+	@ProvidesState(ReturnListStudent.class)
+	public BasicState<List<BorrowedMaterial>> returnListStudent = new BasicState<>(List.class);
 	
 	@Inject
 	private DAO<Student> daoStudent;
@@ -21,8 +29,8 @@ public class ReturnViewModel {
 	@Inject
 	private DAO<BorrowedMaterial> daoBorrowedMaterial;
 	
-	@HandlesAction(getReturnList.class)
-	public List<BorrowedMaterial> getRetunList(String studentId) {
+	@HandlesAction(GenerateReturnList.class)
+	public void generateRetunList(String studentId) {
 		List<BorrowedMaterial> toReturn = new ArrayList<BorrowedMaterial>();
 
 		Student student = daoStudent.find(Integer.parseInt(studentId));
@@ -30,12 +38,12 @@ public class ReturnViewModel {
 		List<BorrowedMaterial> borrowedList = student.getBorrowedList();
 		for (BorrowedMaterial borrowedMaterial : borrowedList) {
 			if(borrowedMaterial.isReceived() //book is received 
-					&& borrowedMaterial.getReturnDate() != null //book hasn't returned yet
+					&& borrowedMaterial.getReturnDate() == null //book hasn't returned yet
 					&& borrowedMaterial.getTeachingMaterial().getToGrade() <= student.getGrade().getGrade() + 1) { //book isn't needed next year
 				toReturn.add(borrowedMaterial);
 			}
 		}
 		
-		return toReturn;
+		returnListStudent.set(toReturn);
 	}
 }
