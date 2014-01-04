d3ad694e6d7f8ebebaaca3cb92caee0d972732be
BennyRa <bennyra92@gmail.com>
2014-01-04 18:35:02 +0100

    Update of already existing students is possible now. Implemented code to persist parent objects of students.

--- src/main/java/de/dhbw/humbuch/util/StudentPersistenceHandler.java
+++ src/main/java/de/dhbw/humbuch/util/StudentPersistenceHandler.java
@@ -18,53 +18,56 @@ public class StudentPersistenceHandler {
 			DAO<Grade> daoGrade, DAO<Parent> daoParent){
 		
 		Iterator<Student> studentIterator = students.iterator();
-		int gradeId = 0;
 		
 		while(studentIterator.hasNext()){
 			Student student = studentIterator.next();
-
-			Collection<Grade> grades = daoGrade.findAllWithCriteria(
-					Restrictions.and(
-							Restrictions.like("grade", student.getGrade().getGrade()),
-							Restrictions.like("suffix", student.getGrade().getSuffix())
-					));
 			
-			Collection<Grade> allGrades = daoGrade.findAll();
-			Iterator<Grade> allGradesIterator = allGrades.iterator();
-			while(allGradesIterator.hasNext()){
-				Grade grade = allGradesIterator.next();
-				gradeId = grade.getId() + 1;
-			}
-						
-			//if grades is not empty (size != 0), use the already existing grade for student
-			if(grades.size() == 0){
-				System.out.println("Here!1");
-				
-				Grade grade = student.getGrade();
-				grade.setId(gradeId);
-				daoGrade.insert(grade);
-
-				Collection<Grade> justInsertedGrades = daoGrade.findAllWithCriteria(
+			Student persistedStudent = daoStudent.find(student.getId());
+			
+			if(persistedStudent == null){			
+				Collection<Grade> grades = daoGrade.findAllWithCriteria(
 						Restrictions.and(
-								Restrictions.like("grade", student.getGrade().getGrade()),
+								Restrictions.eq("grade", student.getGrade().getGrade()),
 								Restrictions.like("suffix", student.getGrade().getSuffix())
 						));
+	
+				if(grades.size() == 1){
+					Iterator<Grade> gradesIterator = grades.iterator();
+					Grade grade = gradesIterator.next();		
+					student.setGrade(grade);
+				}
 				
-				Iterator<Grade> justInsertedGradesIterator = justInsertedGrades.iterator();
-
-				grade = justInsertedGradesIterator.next();
-				student.setGrade(grade);
+	//			Collection<Parent> parents = daoParent.findAllWithCriteria(
+	//					Restrictions.and(
+	//							Restrictions.like("title", student.getParent().getTitle()),
+	//							Restrictions.like("firstname", student.getParent().getFirstname()),
+	//							Restrictions.like("lastname", student.getParent().getLastname()),
+	//							Restrictions.like("street", student.getParent().getStreet()),
+	//							Restrictions.eq("postcode", student.getParent().getPostcode()),
+	//							Restrictions.like("city", student.getParent().getCity())
+	//					));
+	//
+	//			if(parents.size() == 1){
+	//				Iterator<Parent> parentsIterator = parents.iterator();
+	//				Parent parent = parentsIterator.next();		
+	//				student.setParent(parent);
+	//			}
+	//			
+				Parent parent = daoParent.find(1);
+				student.setParent(parent);
+				student.setProfile(null);
+				daoStudent.insert(student);	
 			}
-			else if(grades.size() == 1){
-				Iterator<Grade> gradesIterator = grades.iterator();
-				Grade grade = gradesIterator.next();		
-				student.setGrade(grade);
+			else{
+				persistedStudent.setBirthday(student.getBirthday());
+				persistedStudent.setFirstname(student.getFirstname());
+				persistedStudent.setGender(student.getGender());
+				persistedStudent.setGrade(student.getGrade());
+				persistedStudent.setLastname(student.getLastname());
+				persistedStudent.setParent(student.getParent());
+				persistedStudent.setProfile(null);
+				daoStudent.update(persistedStudent);
 			}
-				
-			Parent parent = daoParent.find(1);
-			student.setParent(parent);
-			student.setProfile(null);
-			daoStudent.insert(student);		
 		}		
 	}
 }
