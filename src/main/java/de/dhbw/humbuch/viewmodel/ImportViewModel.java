89ccce333d6f1d27f407f83e7278a5139a0c086d
BennyRa <bennyra92@gmail.com>
2014-01-08 15:18:02 +0100

    Persistierung des Eltern-Objekts wieder einkommentiert.

--- src/main/java/de/dhbw/humbuch/viewmodel/ImportViewModel.java
+++ src/main/java/de/dhbw/humbuch/viewmodel/ImportViewModel.java
@@ -3,6 +3,11 @@ package de.dhbw.humbuch.viewmodel;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+import org.hibernate.criterion.Restrictions;
 
 import au.com.bytecode.opencsv.CSVReader;
 
@@ -75,5 +80,53 @@ public class ImportViewModel {
 		InputStreamReader inputStream = new InputStreamReader(new ByteArrayInputStream(outputStream.toByteArray()));
 		CSVReader reader = new CSVReader(inputStream);
 		CSVHandler.createStudentObjectsFromCSV(reader);
+	}
+	
+	public static void persistStudents(ArrayList<Student> students, DAO<Student> daoStudent, 
+			DAO<Grade> daoGrade, DAO<Parent> daoParent){
+		
+		Iterator<Student> studentIterator = students.iterator();
+		
+		while(studentIterator.hasNext()){
+			Student student = studentIterator.next();
+			
+			Student persistedStudent = daoStudent.find(student.getId());
+			
+			Collection<Grade> grades = daoGrade.findAllWithCriteria(
+					Restrictions.and(
+							Restrictions.like("grade", student.getGrade().getGrade()),
+							Restrictions.like("suffix", student.getGrade().getSuffix())
+					));
+
+			if(grades.size() == 1){
+				Iterator<Grade> gradesIterator = grades.iterator();
+				Grade grade = gradesIterator.next();		
+				student.setGrade(grade);
+			}
+			
+			if(persistedStudent == null){			
+				
+				Collection<Parent> parents = daoParent.findAllWithCriteria(
+						Restrictions.and(
+								Restrictions.like("title", student.getParent().getTitle()),
+								Restrictions.like("firstname", student.getParent().getFirstname()),
+								Restrictions.like("lastname", student.getParent().getLastname()),
+								Restrictions.like("street", student.getParent().getStreet()),
+								Restrictions.eq("postcode", student.getParent().getPostcode()),
+								Restrictions.like("city", student.getParent().getCity())
+						));
+	
+				if(parents.size() == 1){
+					Iterator<Parent> parentsIterator = parents.iterator();
+					Parent parent = parentsIterator.next();		
+					student.setParent(parent);
+				}
+		
+				daoStudent.insert(student);	
+			}
+			else{
+				daoStudent.update(student);
+			}
+		}		
 	}
 }
