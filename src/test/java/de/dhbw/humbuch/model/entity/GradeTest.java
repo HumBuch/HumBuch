cda8ac3dfc56795149d60c841f995edd3f0edb3b
BennyRa <bennyra92@gmail.com>
2014-01-08 17:31:16 +0100

    Adapted GradeTest for new PDFGrade implementation with map.

--- src/test/java/de/dhbw/humbuch/model/entity/GradeTest.java
+++ src/test/java/de/dhbw/humbuch/model/entity/GradeTest.java
@@ -6,6 +6,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Locale;
@@ -23,91 +24,36 @@ public class GradeTest {
 	
 	@Test
 	public void testGetAllRentedBooksOfGrade(){
-		Grade grade = prepareGradeTest();
-		List<MapperAmountAndBorrowedMaterial> maabm = GradeHandler.getAllRentedBooksOfGrade(grade);
-		
-		assertEquals(2, maabm.get(0).getAmount());
-		assertEquals("Bio1 - Bugs", maabm.get(0).getBorrowedMaterial().getTeachingMaterial().getName());
-		assertEquals(1, maabm.get(1).getAmount());
-		assertEquals("German1 - Faust", maabm.get(1).getBorrowedMaterial().getTeachingMaterial().getName());
-		assertEquals(2, maabm.get(2).getAmount());
-		assertEquals("Java rocks", maabm.get(2).getBorrowedMaterial().getTeachingMaterial().getName());
-		assertEquals(1, maabm.get(3).getAmount());
-		assertEquals("Geometrie for Dummies", maabm.get(3).getBorrowedMaterial().getTeachingMaterial().getName());
+		Map<Grade, Map<TeachingMaterial, Integer>> gradeMap = prepareGradeTest();
+		for(Grade grade : gradeMap.keySet()){
+			List<MapperAmountAndBorrowedMaterial> maabm = GradeHandler.getAllRentedBooksOfGrade(grade);
+			Map<TeachingMaterial, Integer> teachingMaterialMap = gradeMap.get(grade);
+			Iterator<TeachingMaterial> iterator = teachingMaterialMap.keySet().iterator();
+			TeachingMaterial teachingMaterial;
+			if(iterator.hasNext()){
+				teachingMaterial = iterator.next();
+				assertEquals(4, (int) teachingMaterialMap.get(teachingMaterial));
+				assertEquals("Bio1 - Bugs", teachingMaterial.getName());
+			}
+			if(iterator.hasNext()){
+				teachingMaterial = iterator.next();
+				assertEquals(2, (int) teachingMaterialMap.get(teachingMaterial));
+				assertEquals("German1 - Faust", teachingMaterial.getName());
+			}
+			if(iterator.hasNext()){
+				teachingMaterial = iterator.next();
+				assertEquals(3, (int) teachingMaterialMap.get(teachingMaterial));
+				assertEquals("Java rocks", teachingMaterial.getName());
+			}
+			if(iterator.hasNext()){
+				teachingMaterial = iterator.next();
+				assertEquals(7, (int) teachingMaterialMap.get(teachingMaterial));
+				assertEquals("Geometrie for Dummies", teachingMaterial.getName());
+			}
+		}
 	}
 	
-	public static Grade prepareGradeTest(){
-		Grade grade = new Grade.Builder("7b").teacher("Herr Bob").build();
-		List<Student> studentsList = new ArrayList<Student>();
-
-		Set<Subject> profileTypeSet = SubjectHandler.createProfile(new String[]{"L", "E", ""}, "ev");
-		Date date = null;
-		try {
-			date = new SimpleDateFormat("dd.mm.yyyy", Locale.GERMAN).parse("12.04.1970");
-		}
-		catch (ParseException e) {
-			System.err.println("Could not format date " + e.getStackTrace());
-		}		
-		Student student = new Student.Builder(2, "Karl", "August", date, grade).gender("m").profile(profileTypeSet).build();
-		
-		List<BorrowedMaterial> borrowedMaterialList = new ArrayList<BorrowedMaterial>();
-		Category category = new Category.Builder("Book").build();
-		TeachingMaterial teachingMaterial = new TeachingMaterial.Builder(category, "Bio1 - Bugs", "1-2-3", date).price(79.75).toGrade(6).build();	
-		BorrowedMaterial borrowedMaterial = new BorrowedMaterial();
-		borrowedMaterial.setTeachingMaterial(teachingMaterial);
-		borrowedMaterialList.add(borrowedMaterial);
-		
-		teachingMaterial = new TeachingMaterial.Builder(category, "German1 - Faust", "1-2-3", date).price(22.49).toGrade(11).build();	
-		borrowedMaterial = new BorrowedMaterial();
-		borrowedMaterial.setTeachingMaterial(teachingMaterial);
-		borrowedMaterialList.add(borrowedMaterial);
-		
-		teachingMaterial = new TeachingMaterial.Builder(category, "Java rocks", "1-2-3", date).price(22.49).toGrade(11).build();
-		borrowedMaterial = new BorrowedMaterial();
-		borrowedMaterial.setTeachingMaterial(teachingMaterial);
-		borrowedMaterialList.add(borrowedMaterial);
-		
-		student.setBorrowedList(borrowedMaterialList);
-		studentsList.add(student);
-		
-		profileTypeSet = SubjectHandler.createProfile(new String[]{"E", "", "F"}, "rk");
-		try {
-			date = new SimpleDateFormat("dd.mm.yyyy", Locale.GERMAN).parse("12.04.1981");
-		}
-		catch (ParseException e) {
-			System.err.println("Could not format date " + e.getStackTrace());
-		}
-		student = new Student.Builder(5, "Karla", "Kolumna", date, grade).gender("w").profile(profileTypeSet).build();
-		
-		borrowedMaterialList = new ArrayList<BorrowedMaterial>();	
-		
-		//subject = SubjectHandler.createSubject("Biology");
-		teachingMaterial = new TeachingMaterial.Builder(category, "Bio1 - Bugs", "1-2-3", date).price(79.75).toGrade(6).build();
-		borrowedMaterial = new BorrowedMaterial();
-		borrowedMaterial.setTeachingMaterial(teachingMaterial);
-		borrowedMaterialList.add(borrowedMaterial);
-		
-		//subject = SubjectHandler.createSubject("IT");
-		teachingMaterial = new TeachingMaterial.Builder(category, "Java rocks", "1-2-3", date).price(22.49).toGrade(11).build();
-		borrowedMaterial = new BorrowedMaterial();
-		borrowedMaterial.setTeachingMaterial(teachingMaterial);
-		borrowedMaterialList.add(borrowedMaterial);
-		
-		//subject = SubjectHandler.createSubject("Mathe");
-		teachingMaterial = new TeachingMaterial.Builder(category, "Geometrie for Dummies", "1-2-3", date).price(22.49).toGrade(11).build();
-		borrowedMaterial = new BorrowedMaterial();
-		borrowedMaterial.setTeachingMaterial(teachingMaterial);
-		borrowedMaterialList.add(borrowedMaterial);
-
-		student.setBorrowedList(borrowedMaterialList);
-		studentsList.add(student);
-		
-		grade.setStudents(studentsList);
-		
-		return grade;
-	}
-	
-	public static Map<Grade, Map<TeachingMaterial, Integer>> prepareNewGradeTest(){
+	public static Map<Grade, Map<TeachingMaterial, Integer>> prepareGradeTest(){
 		Grade grade = new Grade.Builder("7b").teacher("Herr Bob").build();
 		Map<Grade, Map<TeachingMaterial, Integer>> gradeMap = new LinkedHashMap<>();
 		Map<TeachingMaterial, Integer> teachingMaterialMap = new LinkedHashMap<>();
@@ -122,13 +68,16 @@ public class GradeTest {
 		TeachingMaterial teachingMaterial = new TeachingMaterial.Builder(category, "Bio1 - Bugs", "1-2-3", date).price(79.75).toGrade(6).build();
 		teachingMaterial.setId(1);
 		teachingMaterialMap.put(teachingMaterial, 4);
-		teachingMaterial = new TeachingMaterial.Builder(category, "Geometrie for Dummies", "1-2-3", date).price(22.49).toGrade(11).build();
+		teachingMaterial = new TeachingMaterial.Builder(category, "German1 - Faust", "1-2-3", date).price(22.49).toGrade(11).build();
 		teachingMaterial.setId(2);
-		teachingMaterialMap.put(teachingMaterial, 7);
+		teachingMaterialMap.put(teachingMaterial, 2);
 		teachingMaterial = new TeachingMaterial.Builder(category, "Java rocks", "1-2-3", date).price(22.49).toGrade(11).build();
 		teachingMaterial.setId(3);
-		teachingMaterialMap.put(teachingMaterial, 2);
+		teachingMaterialMap.put(teachingMaterial, 3);
 		gradeMap.put(grade, teachingMaterialMap);
+		teachingMaterial = new TeachingMaterial.Builder(category, "Geometrie for Dummies", "1-2-3", date).price(22.49).toGrade(11).build();
+		teachingMaterial.setId(4);
+		teachingMaterialMap.put(teachingMaterial, 7);
 
 		return gradeMap;		
 	}
--- src/test/java/de/dhbw/humbuch/util/PDFTest.java
+++ src/test/java/de/dhbw/humbuch/util/PDFTest.java
@@ -64,7 +64,7 @@ public class PDFTest {
 //		Grade grade = GradeTest.prepareGradeTest();
 //		new PDFClassList(grade).savePDF("./testfiles/FirstPdfClass.pdf");
 		
-		Map<Grade, Map<TeachingMaterial, Integer>> gradeMap = GradeTest.prepareNewGradeTest();
+		Map<Grade, Map<TeachingMaterial, Integer>> gradeMap = GradeTest.prepareGradeTest();
 		new PDFClassList(gradeMap).savePDF("./testfiles/SecondPdfClass.pdf");
 	}
 	
