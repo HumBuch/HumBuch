a450037a92a68483af60a673ab6fe0342a5e7fa3
jidel <Johannes@172.16.52.228>
2014-01-08 16:49:13 +0100

    Limit max file size of the uploaded file

--- src/main/java/de/dhbw/humbuch/view/ImportView.java
+++ src/main/java/de/dhbw/humbuch/view/ImportView.java
@@ -7,9 +7,11 @@ import java.util.NoSuchElementException;
 import com.google.inject.Inject;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+import com.vaadin.server.Page;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.Notification;
 import com.vaadin.ui.Upload;
-import com.vaadin.ui.Upload.FailedEvent;
+import com.vaadin.ui.Upload.Receiver;
 import com.vaadin.ui.VerticalLayout;
 
 import de.davherrmann.mvvm.BasicState;
@@ -19,18 +21,17 @@ import de.davherrmann.mvvm.annotations.BindState;
 import de.dhbw.humbuch.viewmodel.ImportViewModel;
 import de.dhbw.humbuch.viewmodel.ImportViewModel.ImportResult;
 
-
 public class ImportView extends VerticalLayout implements View, ViewInformation {
 
 	private static final long serialVersionUID = -739081142499192817L;
 
 	private static final String TITLE = "Schüler Import";
-	
+
 	private Upload upload;
-	private UploadReceiver receiver = new UploadReceiver();
+	private UploadReceiver receiver = new UploadReceiver(5242880); // 5MB = 5242880
 
 	private Label labelResult;
-	
+
 	protected ImportViewModel importViewModel;
 
 	@BindState(ImportResult.class)
@@ -94,29 +95,54 @@ public class ImportView extends VerticalLayout implements View, ViewInformation 
 		return TITLE;
 	}
 
-	public class UploadReceiver implements Upload.Receiver,
-			Upload.SucceededListener, Upload.FailedListener {
+	public class UploadReceiver implements Receiver, Upload.SucceededListener,
+			Upload.ProgressListener {
 
 		private static final long serialVersionUID = 1L;
-
 		private ByteArrayOutputStream outputStream;
-		//private final long maxFileSize = 10;
+		private final long maxSize;
+		private boolean interrupted;
+
+		/**
+		 * @param maxSize
+		 *            The maximum file size that will be accepted (in
+		 *            bytes). -1 in case of no limit. 100Kb = 100000
+		 */
+		public UploadReceiver(long maxSize) {
+			this.maxSize = maxSize;
+		}
 
 		public OutputStream receiveUpload(String filename, String MIMEType) {
 
-		    this.outputStream = new ByteArrayOutputStream();
-		    return outputStream;
-		    
+			this.outputStream = new ByteArrayOutputStream();
+			return outputStream;
+
 		}
 
 		public void uploadSucceeded(Upload.SucceededEvent event) {
-			importViewModel.receiveUploadByteOutputStream(outputStream);
+			if (!interrupted) {
+				importViewModel.receiveUploadByteOutputStream(outputStream);
+			}
+		}
+
+		/**
+		 * Interrupts the current upload
+		 */
+		protected void interrupt() {
+			upload.interruptUpload();
+			interrupted = true;
+			new Notification(
+					"Import nicht möglich.",
+					"Die ausgewählte Datei ist zu groß. Bitte kontaktieren Sie einen Administrator.",
+					Notification.Type.WARNING_MESSAGE).show(Page.getCurrent());
 		}
 
 		@Override
-		public void uploadFailed(FailedEvent event) {
-			// TODO Auto-generated method stub
-			
+		public void updateProgress(long readBytes, long contentLength) {
+			if (readBytes > maxSize || contentLength > maxSize) {
+				interrupt();
+			}
+
 		}
 	}
 }
