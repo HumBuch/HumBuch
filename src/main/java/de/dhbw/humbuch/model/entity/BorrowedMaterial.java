eba74df14d48b01d10f40234afab912d8e54e228
davvitt <vitt.da@gmail.com>
2014-01-09 15:55:50 +0100

    bugfix

--- src/main/java/de/dhbw/humbuch/model/entity/BorrowedMaterial.java
+++ src/main/java/de/dhbw/humbuch/model/entity/BorrowedMaterial.java
@@ -2,8 +2,11 @@ package de.dhbw.humbuch.model.entity;
 
 import java.util.Date;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -14,13 +17,14 @@ import javax.persistence.Table;
 public class BorrowedMaterial implements de.dhbw.humbuch.model.entity.Entity {
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
 	
-	@ManyToOne(fetch=FetchType.LAZY)
+	@ManyToOne(fetch=FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
 	@JoinColumn(name="studentId", referencedColumnName="id")
 	private Student student;
 	
-	@ManyToOne(fetch=FetchType.LAZY)
+	@ManyToOne(fetch=FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
 	@JoinColumn(name="teachingMaterialId", referencedColumnName="id")
 	private TeachingMaterial teachingMaterial;
 	
