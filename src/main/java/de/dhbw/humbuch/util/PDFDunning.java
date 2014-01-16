5317f115ad5c478688e79212b681978d826e765b
BennyRa <bennyra92@gmail.com>
2014-01-17 00:49:40 +0100

    Merged changes of PDF classes into develop branch. Layout and content of PDFs are changed.

--- src/main/java/de/dhbw/humbuch/util/PDFDunning.java
+++ src/main/java/de/dhbw/humbuch/util/PDFDunning.java
@@ -8,83 +8,109 @@ import com.lowagie.text.Document;
 import com.lowagie.text.DocumentException;
 import com.lowagie.text.pdf.PdfPTable;
 
-import de.dhbw.humbuch.model.SubjectHandler;
 import de.dhbw.humbuch.model.entity.BorrowedMaterial;
 import de.dhbw.humbuch.model.entity.Student;
 
 
 public class PDFDunning extends PDFHandler {
+
 	Student student;
 	Set<Student> students;
 	boolean secondDunning;
 	List<BorrowedMaterial> borrowedMaterials;
-	
-	private PDFDunning(){};
-	
-	public static PDFDunning createFirstDunning(Set<Student> students, List<BorrowedMaterial> borrowedMaterials){
+	Set<List<BorrowedMaterial>> borrowedMaterialsSet;
+
+	private PDFDunning() {
+	};
+
+	public static PDFDunning createFirstDunning(Set<List<BorrowedMaterial>> borrowedMaterials) {
 		PDFDunning pdfDunning = new PDFDunning();
-		pdfDunning.students = students;
-		pdfDunning.borrowedMaterials = borrowedMaterials;
+		pdfDunning.borrowedMaterialsSet = borrowedMaterials;
 		pdfDunning.secondDunning = false;
 		return pdfDunning;
 	}
-	
-	public static PDFDunning createSecondDunning(Set<Student> students, List<BorrowedMaterial> borrowedMaterials){
+
+	public static PDFDunning createSecondDunning(Set<List<BorrowedMaterial>> borrowedMaterials) {
 		PDFDunning pdfDunning = new PDFDunning();
-		pdfDunning.students = students;
-		pdfDunning.borrowedMaterials = borrowedMaterials;
+		pdfDunning.borrowedMaterialsSet = borrowedMaterials;
 		pdfDunning.secondDunning = true;
 		return pdfDunning;
 	}
 
 	protected void insertDocumentParts(Document document) {
-		for(Student student : this.students){
-			this.addHeading(document, "Mahnung 2013");
-			this.student = student;
-			this.addStudentInformation(document);
-			this.addContent(document);
-			this.addSignatureField(document, "Schüler");
-			document.newPage();
-			this.resetPageNumber();
+		for (List<BorrowedMaterial> borrowedMaterials : this.borrowedMaterialsSet) {
+			this.addHeading(document);
+			if(borrowedMaterials.size() != 0){
+				this.borrowedMaterials = borrowedMaterials;
+				this.student = borrowedMaterials.get(0).getStudent();
+				if(this.secondDunning){
+					this.addParentInformation(document);
+				}
+				this.addStudentInformation(document);
+				this.addInformationAboutDocument(document, "Mahnungs-Liste");
+				this.addContent(document);
+				document.newPage();
+				this.resetPageNumber();
+			}	
 		}
 	}
 
 	protected void addContent(Document document) {
 		PdfPTable table = PDFHandler.createMyStandardTable(1);
 		String dunningText = "";
-		if(!this.secondDunning){
-			dunningText = "Wir bitten darum, folgende Bücher zurückzugeben: \n";
+		if (!this.secondDunning) {
+			dunningText = "Wir bitten darum, folgende Bücher innerhalb von 2 Wochen zurückzugeben oder Ersatz zu beschaffen:";
 		}
-		else{
-			dunningText = "Sehr geehrte/r, " + student.getParent().getTitle() + " " + student.getParent().getLastname()  + "\n\n"
+		else {
+			dunningText = "Sehr geehrte/r " + student.getParent().getTitle() + " " + student.getParent().getLastname() + ",\n\n"
 					+ "leider müssen wir mitteilen, dass " + student.getFirstname() + " trotz bereits erfolgter Mahnung die unten aufgelisteten"
-							+ " Bücher nicht zurückgegeben hat. Wir bitten daher um schnellstmögliche Rückgabe. \n\n"
-							+ "Mit freundlichen Grüßen \n"
-							+ "Schulverwaltung";
+					+ " Bücher nicht zurückgegeben hat. Wir bitten darum, folgende Bücher innerhalb von 2 Wochen zurückzugeben oder Ersatz zu beschaffen. \n\n"
+					+ "Mit freundlichen Grüßen \n"
+					+ "Schulverwaltung";
 		}
 		PDFHandler.fillTableWithContent(table, false,
-				new String[]{dunningText}, false);
+				new String[] { dunningText }, false);
 		try {
 			document.add(table);
 		}
 		catch (DocumentException e) {
 			e.printStackTrace();
 		}
-		
+
 		table = this.createTableWithRentalInformationHeader();
-		
+
 		Iterator<BorrowedMaterial> iterator = this.borrowedMaterials.iterator();
 		BorrowedMaterial borrowedMaterial;
-		while(iterator.hasNext()){
+		while (iterator.hasNext()) {
 			borrowedMaterial = (BorrowedMaterial) iterator.next();
-			String[] contentArray = {//borrowedMaterial.getTeachingMaterial().getSubject().getName(),
-			                         ""+borrowedMaterial.getTeachingMaterial().getToGrade(),
-			                         borrowedMaterial.getTeachingMaterial().getName(),
-			                      	 "" };
-			PDFHandler.fillTableWithContent(table, true, contentArray);		
+			String[] contentArray = { borrowedMaterial.getTeachingMaterial().getName(),
+										"" + borrowedMaterial.getTeachingMaterial().getToGrade(),
+										"" };
+			PDFHandler.fillTableWithContentWithoutSpace(table, true, contentArray, true, 5f);
 		}
-	    
-	    try {
+
+		try {
+			document.add(table);
+		}
+		catch (DocumentException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Inserts information about the student like grade, language, name etc.
+	 * 
+	 * @param document
+	 *            represents the PDF before it is saved
+	 */
+	private void addStudentInformation(Document document) {
+		PdfPTable table = PDFHandler.createMyStandardTable(2, new float[] { 1f, 6f });
+
+		String[] contentArray = { "Schüler: ", this.student.getFirstname() + " " + this.student.getLastname(),
+									"Klasse: ", "" + this.student.getGrade().toString() + "\n" };
+		PDFHandler.fillTableWithContentWithoutSpace(table, false, contentArray, false, 0f);
+
+		try {
 			document.add(table);
 		}
 		catch (DocumentException e) {
@@ -92,21 +118,14 @@ public class PDFDunning extends PDFHandler {
 		}
 	}
 	
-	/**
-	 * Inserts information about the student like grade, language, name etc.
-	 * 
-	 * @param document represents the PDF before it is saved
-	 */	
-	private void addStudentInformation(Document document){
-		PdfPTable table = PDFHandler.createMyStandardTable(2, new float[]{1f, 6f});
+	private void addParentInformation(Document document){
+		PdfPTable table = PDFHandler.createMyStandardTable(1);
 
-		String[] contentArray = {"Schüler: ", this.student.getFirstname() + " " + this.student.getLastname(),
-		                         "Klasse: ", "" + this.student.getGrade().toString(),
-		                         "Schuljahr: ", "#SCHOOLYEAR",
-		                         "Sprachen: ", SubjectHandler.getLanguageProfile(this.student.getProfile()),
-					             "Religion: ", SubjectHandler.getReligionProfile(this.student.getProfile()) + "\n"};
-		PDFHandler.fillTableWithContentWithoutSpace(table, false, contentArray);
-		
+		String[] contentArray = { this.student.getParent().getTitle() + " " + this.student.getParent().getFirstname() + " " + this.student.getParent().getLastname(),
+		                          this.student.getParent().getStreet(),
+		                          this.student.getParent().getCity() + " " + this.student.getParent().getPostcode() + "\n" };
+		PDFHandler.fillTableWithContentWithoutSpace(table, false, contentArray, false, 0f);
+
 		try {
 			document.add(table);
 			PDFHandler.addEmptyLineToDocument(document, 1);
--- src/main/java/de/dhbw/humbuch/util/PDFStudentList.java
+++ src/main/java/de/dhbw/humbuch/util/PDFStudentList.java
@@ -6,81 +6,85 @@ import java.util.Set;
 
 import com.lowagie.text.Document;
 import com.lowagie.text.DocumentException;
+import com.lowagie.text.Font;
+import com.lowagie.text.FontFactory;
 import com.lowagie.text.pdf.PdfPTable;
 
 import de.dhbw.humbuch.model.SubjectHandler;
 import de.dhbw.humbuch.model.entity.BorrowedMaterial;
 import de.dhbw.humbuch.model.entity.Student;
 
-public final class PDFStudentList extends PDFHandler{
+
+public final class PDFStudentList extends PDFHandler {
+
 	private Student student;
 	private List<BorrowedMaterial> borrowedMaterialList;
 	private List<BorrowedMaterial> returnList;
 	private List<BorrowedMaterial> lendingList;
-	
+
 	private Set<Builder> builders;
-	
+
 	/**
 	 * The content of the PDFs is printed in the order of the students-set.
+	 * 
 	 * @param builder
 	 */
-	public PDFStudentList(Builder... builder){
+	public PDFStudentList(Builder... builder) {
 		super();
 		this.builders = new LinkedHashSet<>();
-		for(Builder b : builder){
+		for (Builder b : builder) {
 			this.builders.add(b);
 		}
 	}
-	
-	public PDFStudentList(Set<Builder> builder){
+
+	public PDFStudentList(Set<Builder> builder) {
 		super();
 		this.builders = builder;
 	}
-	
-	protected void insertDocumentParts(Document document){
-		for(Builder builder : builders){
-			if(builder.student != null){
+
+	protected void insertDocumentParts(Document document) {
+		for (Builder builder : builders) {
+			if (builder.student != null) {
 				this.student = builder.student;
 			}
-			else{
+			else {
 				continue;
 			}
 			this.borrowedMaterialList = builder.borrowedMaterialList;
 			this.lendingList = builder.lendingList;
 			this.returnList = builder.returnList;
 
-			this.addHeading(document, "Ausgabe-Liste 2013");
+			this.addHeading(document);
 			this.addStudentInformation(document);
+			this.addInformationAboutDocument(document, "Informations-Liste");
 			this.addContent(document);
-			this.addRentalDisclosure(document);
-			this.addSignatureField(document, "Schüler");
 			document.newPage();
 			this.resetPageNumber();
-		}		
+		}
 	}
-	
+
 	protected void addContent(Document document) {
-		if(this.borrowedMaterialList != null && !this.borrowedMaterialList.isEmpty()){
+		if (this.borrowedMaterialList != null && !this.borrowedMaterialList.isEmpty()) {
 			PdfPTable table = PDFHandler.createMyStandardTable(1);
-			PDFHandler.fillTableWithContent(table, false,
-					new String[]{"\nDie folgenden Bücher befinden sich im Besitz des Schülers/der Schülerin: \n"}, false);
-			 try {
-					document.add(table);
+			PDFHandler.fillTableWithContentWithoutAlignment(table, false,
+					new String[] { "\nDie folgenden Bücher befinden sich im Besitz des Schülers/der Schülerin:" }, 
+					FontFactory.getFont("Helvetica", 10, Font.BOLD));
+			try {
+				document.add(table);
 			}
 			catch (DocumentException e) {
 				e.printStackTrace();
 			}
-			
+
 			table = this.createTableWithRentalInformationHeader();
 
-			for(BorrowedMaterial borrowedMaterial : this.borrowedMaterialList){
-				String[] contentArray = {//borrowedMaterial.getTeachingMaterial().getSubject().getName(),
-				                         ""+borrowedMaterial.getTeachingMaterial().getToGrade(),
-				                         borrowedMaterial.getTeachingMaterial().getName(),
-				                      	 "" };
-				PDFHandler.fillTableWithContent(table, true, contentArray);		
+			for (BorrowedMaterial borrowedMaterial : this.borrowedMaterialList) {
+				String[] contentArray = { borrowedMaterial.getTeachingMaterial().getName(),
+											"" + borrowedMaterial.getTeachingMaterial().getToGrade(),
+											"" };
+				PDFHandler.fillTableWithContentWithoutSpace(table, true, contentArray, true, 5f);
 			}
-		    try {
+			try {
 				document.add(table);
 				PDFHandler.addEmptyLineToDocument(document, 1);
 			}
@@ -88,100 +92,105 @@ public final class PDFStudentList extends PDFHandler{
 				e.printStackTrace();
 			}
 		}
-	    if(this.returnList != null && !this.returnList.isEmpty()){
-	    	PdfPTable table = PDFHandler.createMyStandardTable(1);
-			PDFHandler.fillTableWithContent(table, false,
-					new String[]{"\n Die folgenden Bücher müssen zurückgegeben werden: \n"}, false);
-			 try {
-					document.add(table);
+		if (this.returnList != null && !this.returnList.isEmpty()) {
+			PdfPTable table = PDFHandler.createMyStandardTable(1);
+			PDFHandler.fillTableWithContentWithoutAlignment(table, false,
+					new String[] { "\n Die folgenden Bücher müssen zurückgegeben werden:" }, 
+					FontFactory.getFont("Helvetica", 10, Font.BOLD));
+			try {
+				document.add(table);
 			}
 			catch (DocumentException e) {
 				e.printStackTrace();
 			}
-	    	
-	    	table = this.createTableWithRentalInformationHeader();
-			
-	    	for(BorrowedMaterial borrowedMaterial : this.returnList){				
-				String[] contentArray = {//borrowedMaterial.getTeachingMaterial().getSubject().getName(),
-				                         ""+borrowedMaterial.getTeachingMaterial().getToGrade(),
-				                         borrowedMaterial.getTeachingMaterial().getName(),
-				                      	 "" };
-				PDFHandler.fillTableWithContent(table, true, contentArray);		
+
+			table = this.createTableWithRentalInformationHeader();
+
+			for (BorrowedMaterial borrowedMaterial : this.returnList) {
+				String[] contentArray = { borrowedMaterial.getTeachingMaterial().getName(),
+											"" + borrowedMaterial.getTeachingMaterial().getToGrade(),
+											"" };
+
+				PDFHandler.fillTableWithContentWithoutSpace(table, true, contentArray, true, 5f);
+
 			}
-		    
-		    try {
+
+			try {
 				document.add(table);
+				this.addRentalDisclosure(document);
+				this.addSignatureField(document, "Lehrer");
 				PDFHandler.addEmptyLineToDocument(document, 1);
 			}
 			catch (DocumentException e) {
 				e.printStackTrace();
 			}
-	    }
-	    if(this.lendingList != null && !this.lendingList.isEmpty()){
-	    	PdfPTable table = PDFHandler.createMyStandardTable(1);
-			PDFHandler.fillTableWithContent(table, false,
-					new String[]{"\n Die folgenden Bücher sollen ausgeliehen werden: \n"}, false);
-			 try {
-					document.add(table);
-			}
-			catch (DocumentException e) {
-				e.printStackTrace();
-			}	    	
-	    	
-	    	table = this.createTableWithRentalInformationHeader();
-	    	
-	    	for(BorrowedMaterial borrowedMaterial : this.lendingList){
-				String[] contentArray = {//borrowedMaterial.getTeachingMaterial().getSubject().getName(),
-				                         ""+borrowedMaterial.getTeachingMaterial().getToGrade(),
-				                         borrowedMaterial.getTeachingMaterial().getName(),
-				                      	 "" };
-				PDFHandler.fillTableWithContent(table, true, contentArray);		
-			}
-		    
-		    try {
+		}
+		if (this.lendingList != null && !this.lendingList.isEmpty()) {
+			PdfPTable table = PDFHandler.createMyStandardTable(1);
+			PDFHandler.fillTableWithContentWithoutAlignment(table, false,
+					new String[] { "\n Die folgenden Bücher sollen ausgeliehen werden:" }, 
+					FontFactory.getFont("Helvetica", 10, Font.BOLD));
+			try {
 				document.add(table);
 			}
 			catch (DocumentException e) {
 				e.printStackTrace();
 			}
-	    }
-	    
+
+			table = this.createTableWithRentalInformationHeader();
+
+			for (BorrowedMaterial borrowedMaterial : this.lendingList) {
+				String[] contentArray = { borrowedMaterial.getTeachingMaterial().getName(),
+											"" + borrowedMaterial.getTeachingMaterial().getToGrade(),
+											"" };
+
+				PDFHandler.fillTableWithContentWithoutSpace(table, true, contentArray, true, 5f);
+			}
+
+			try {
+				document.add(table);
+			}
+			catch (DocumentException e) {
+				e.printStackTrace();
+			}
+		}
+
 	}
-	
+
 	/**
 	 * Inserts information about the student like grade, language, name etc.
 	 * 
-	 * @param document represents the PDF before it is saved
-	 */	
-	private void addStudentInformation(Document document){
-		PdfPTable table = PDFHandler.createMyStandardTable(2, new float[]{1f, 6f});
+	 * @param document
+	 *            represents the PDF before it is saved
+	 */
+	private void addStudentInformation(Document document) {
+		PdfPTable table = PDFHandler.createMyStandardTable(2, new float[] { 1f, 6f });
 
-		String[] contentArray = {"Schüler: ", this.student.getFirstname() + " " + this.student.getLastname(),
-		                         "Klasse: ", "" + this.student.getGrade().toString(),
-		                         "Schuljahr: ", "#SCHOOLYEAR",
-		                         "Sprachen: ", SubjectHandler.getLanguageProfile(this.student.getProfile()),
-					             "Religion: ", SubjectHandler.getReligionProfile(this.student.getProfile()) + "\n"};
-		PDFHandler.fillTableWithContentWithoutSpace(table, false, contentArray);
-		
+		String[] contentArray = { "Schüler: ", this.student.getFirstname() + " " + this.student.getLastname(),
+									"Klasse: ", "" + this.student.getGrade().toString(),
+									"Sprachen: ", SubjectHandler.getLanguageProfile(this.student.getProfile()),
+									"Religion: ", SubjectHandler.getReligionProfile(this.student.getProfile())};
+		PDFHandler.fillTableWithContentWithoutSpace(table, false, contentArray, false, 0f);
+
 		try {
 			document.add(table);
-			PDFHandler.addEmptyLineToDocument(document, 1);
 		}
 		catch (DocumentException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	/**
 	 * 
-	 * @param document represents the PDF before it is saved
+	 * @param document
+	 *            represents the PDF before it is saved
 	 */
-	private void addRentalDisclosure(Document document){
+	private void addRentalDisclosure(Document document) {
 		PdfPTable table = PDFHandler.createMyStandardTable(1);
 		PDFHandler.fillTableWithContent(table, false,
-				new String[]{"\nDie oben angeführten Schulbücher habe ich erhalten.\n" +
-				"Die ausgeliehenen Bücher habe ich auf Vollständigkeit und Beschädigung überprüft. "+
-				"Beschädigte oder verlorengegangene Bücher müssen ersetzt werden.\n"}, false);
+				new String[] { "\nDie oben angeführten Schulbücher hat der Schüler zurückgegeben.\n" +
+						"Die ausgeliehenen Bücher wurden auf Vollständigkeit und Beschädigung überprüft. " +
+						"Beschädigte oder verlorengegangene Bücher wurden ersetzt.\n" }, false);
 		try {
 			document.add(table);
 		}
@@ -189,43 +198,46 @@ public final class PDFStudentList extends PDFHandler{
 			e.printStackTrace();
 		}
 	}
-	
-	public static class Builder{		
+
+
+	public static class Builder {
+
 		private Student student;
 		private List<BorrowedMaterial> borrowedMaterialList;
 		private List<BorrowedMaterial> lendingList;
 		private List<BorrowedMaterial> returnList;
-				
-		public Builder(){}
-		
-		public Builder borrowedMaterialList(List<BorrowedMaterial> borrowedMaterialList){
+
+		public Builder() {
+		}
+
+		public Builder borrowedMaterialList(List<BorrowedMaterial> borrowedMaterialList) {
 			this.borrowedMaterialList = borrowedMaterialList;
-			if(borrowedMaterialList != null){
+			if (borrowedMaterialList != null) {
 				this.student = borrowedMaterialList.get(0).getStudent();
 			}
 			return this;
 		}
-		
-		public Builder lendingList(List<BorrowedMaterial> borrowedMaterialList){
+
+		public Builder lendingList(List<BorrowedMaterial> borrowedMaterialList) {
 			this.lendingList = borrowedMaterialList;
-			if(borrowedMaterialList != null){
+			if (borrowedMaterialList != null) {
 				this.student = borrowedMaterialList.get(0).getStudent();
 			}
 			return this;
 		}
-		
-		public Builder returnList(List<BorrowedMaterial> borrowedMaterialList){
+
+		public Builder returnList(List<BorrowedMaterial> borrowedMaterialList) {
 			this.returnList = borrowedMaterialList;
-			if(borrowedMaterialList != null){
+			if (borrowedMaterialList != null) {
 				this.student = borrowedMaterialList.get(0).getStudent();
 			}
 			return this;
 		}
-		
+
 		@Deprecated
-		public PDFStudentList build(){
+		public PDFStudentList build() {
 			return new PDFStudentList(this);
 		}
-		
+
 	}
 }
