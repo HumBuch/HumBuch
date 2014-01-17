7997b744622506859b2fbaac3374e6040eb5c874
jidel <Johannes@172.16.52.228>
2014-01-18 00:16:19 +0100

    Updated MessageEvent with a caption

--- src/main/java/de/dhbw/humbuch/event/MessageEvent.java
+++ src/main/java/de/dhbw/humbuch/event/MessageEvent.java
@@ -6,6 +6,7 @@ package de.dhbw.humbuch.event;
  */
 public class MessageEvent {
 
+	public final String caption;
 	public final String message;
 	public final Type type;
 
@@ -21,18 +22,34 @@ public class MessageEvent {
 	 *            {@link String} containing the message
 	 */
 	public MessageEvent(String message) {
-		this(message, Type.INFO);
+		this("", message, Type.INFO);
 	}
 
 	/**
-	 * Creates an event with the specified message and type
+	 * Creates an event with the specified caption, message and {@link Type}.INFO as
+	 * standard type
 	 * 
+	 * @param caption
+	 * 			  {@link String} containing the caption
+	 * @param message
+	 *            {@link String} containing the message
+	 */
+	public MessageEvent(String caption, String message) {
+		this(caption, message, Type.INFO);
+	}
+	
+	/**
+	 * Creates an event with the specified caption, message and type
+	 * 
+	 * @param caption
+	 * 			  {@link String} containing the caption
 	 * @param message
 	 *            {@link String} containing the message
 	 * @param type
 	 *            {@link Type} defining the message type
 	 */
-	public MessageEvent(String message, Type type) {
+	public MessageEvent(String caption, String message, Type type) {
+		this.caption = caption;
 		this.message = message;
 		this.type = type;
 	}
