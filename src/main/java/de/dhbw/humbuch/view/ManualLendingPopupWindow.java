3d4fd2f29328e7cfc3394201779475b5919c9934
Henning Muszynski <henningmuszynski@web.de>
2014-01-21 13:57:39 +0100

    Rethought the manual lending process

--- src/main/java/de/dhbw/humbuch/view/ManualLendingPopupWindow.java
+++ src/main/java/de/dhbw/humbuch/view/ManualLendingPopupWindow.java
@@ -2,6 +2,9 @@ package de.dhbw.humbuch.view;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import org.slf4j.Logger;
@@ -22,23 +25,25 @@ import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Table;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Window;
 
+import de.dhbw.humbuch.model.entity.Student;
 import de.dhbw.humbuch.model.entity.TeachingMaterial;
 
 
-public class ManualLendingPopupView extends VerticalLayout implements ViewInformation {
+public class ManualLendingPopupWindow extends Window {
 
 	private static final long serialVersionUID = -6517435259424504689L;
 
-	private static final Logger LOG = LoggerFactory.getLogger(ManualLendingPopupView.class);
+	private static final Logger LOG = LoggerFactory.getLogger(ManualLendingPopupWindow.class);
 
-	private static final String TITLE = "Manuelle Ausleihe";
 	private static final String SEARCH_MATERIALS = "Materialien durchsuchen";
 	private static final String SAVE = "Ausgewählte Materialien ausleihen";
 	private static final String CANCEL = "Manuelle Ausleihe abbrechen";
 	private static final String TEACHING_MATERIAL_HEADER = "Verfügbare Lehrmittel";
 
-	private HorizontalLayout horizontalLayoutButtonBar;
+	private VerticalLayout verticalLayoutContent;
+	private HorizontalLayout horizontalLayoutHeaderBar;
 	private TextField textFieldSearchBar;
 	private Table tableTeachingMaterials;
 	private Button buttonSave;
@@ -47,15 +52,19 @@ public class ManualLendingPopupView extends VerticalLayout implements ViewInform
 	private ArrayList<TeachingMaterial> teachingMaterials;
 	private ArrayList<TeachingMaterial> currentlySelectedMaterials;
 	private LendingView lendingView;
+	private Student selectedStudent;
 
-	public ManualLendingPopupView(LendingView lendingView) {
+	public ManualLendingPopupWindow(LendingView lendingView, Student selectedStudent) {
+		super("Manuelle Ausleihe für " + selectedStudent.getFirstname() + " " + selectedStudent.getLastname());
 		this.lendingView = lendingView;
+		this.selectedStudent = selectedStudent;
 		init();
 		buildLayout();
 	}
 
 	private void init() {
-		horizontalLayoutButtonBar = new HorizontalLayout();
+		verticalLayoutContent = new VerticalLayout();
+		horizontalLayoutHeaderBar = new HorizontalLayout();
 		textFieldSearchBar = new TextField(SEARCH_MATERIALS);
 		tableTeachingMaterials = new Table();
 		buttonSave = new Button(SAVE);
@@ -64,8 +73,6 @@ public class ManualLendingPopupView extends VerticalLayout implements ViewInform
 		containerTableTeachingMaterials = new IndexedContainer();
 
 		buttonSave.setIcon(new ThemeResource("images/icons/16/icon_save_red.png"));
-
-		textFieldSearchBar.setWidth("66%");
 
 		containerTableTeachingMaterials.addContainerProperty(TEACHING_MATERIAL_HEADER, String.class, null);
 
@@ -77,22 +84,29 @@ public class ManualLendingPopupView extends VerticalLayout implements ViewInform
 		setTableListener();
 		updateTableContent();
 
-		setSpacing(true);
-		setMargin(true);
+		horizontalLayoutHeaderBar.setSpacing(true);
+		verticalLayoutContent.setSpacing(true);
+		verticalLayoutContent.setMargin(true);
+
+		center();
 
 		addListeners();
 	}
 
 	private void buildLayout() {
-		horizontalLayoutButtonBar.addComponent(buttonCancel);
-		horizontalLayoutButtonBar.addComponent(buttonSave);
+		horizontalLayoutHeaderBar.addComponent(textFieldSearchBar);
+		horizontalLayoutHeaderBar.addComponent(buttonCancel);
+		horizontalLayoutHeaderBar.addComponent(buttonSave);
 
-		addComponent(textFieldSearchBar);
-		addComponent(tableTeachingMaterials);
-		addComponent(horizontalLayoutButtonBar);
+		verticalLayoutContent.addComponent(horizontalLayoutHeaderBar);
+		verticalLayoutContent.addComponent(tableTeachingMaterials);
+
+		setContent(verticalLayoutContent);
 	}
 
 	private void updateTableContent() {
+		teachingMaterials = lendingView.getTeachingMaterials();
+
 		if (teachingMaterials == null) {
 			return;
 		}
@@ -134,7 +148,7 @@ public class ManualLendingPopupView extends VerticalLayout implements ViewInform
 
 			@Override
 			public void buttonClick(ClickEvent event) {
-				lendingView.closePopup(false);
+				close();
 			}
 		});
 
@@ -144,7 +158,7 @@ public class ManualLendingPopupView extends VerticalLayout implements ViewInform
 
 			@Override
 			public void buttonClick(ClickEvent event) {
-				lendingView.closePopup(true);
+				saveTeachingMaterialsForStudent();
 			}
 		});
 
@@ -161,17 +175,15 @@ public class ManualLendingPopupView extends VerticalLayout implements ViewInform
 		});
 	}
 
-	public ArrayList<TeachingMaterial> getCurrentlySelectedTeachingMaterials() {
-		return currentlySelectedMaterials;
-	}
-
-	public void updateTeachingMaterials(Collection<TeachingMaterial> teachingMaterials) {
+	public void setTeachingMaterials(Collection<TeachingMaterial> teachingMaterials) {
 		this.teachingMaterials = new ArrayList<TeachingMaterial>(teachingMaterials);
 		updateTableContent();
 	}
 
-	@Override
-	public String getTitle() {
-		return TITLE;
+	private void saveTeachingMaterialsForStudent() {
+		Map<Student, List<TeachingMaterial>> studentsWithMaterials = new HashMap<Student, List<TeachingMaterial>>();
+		studentsWithMaterials.put(selectedStudent, currentlySelectedMaterials);
+
+		lendingView.saveTeachingMaterialsForStudents(studentsWithMaterials);
 	}
 }
