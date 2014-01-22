97d32d17b37c44a6fde01c5dec5052f20842f7a1
jidel <Johannes@172.16.52.228>
2014-01-22 11:38:36 +0100

    Add validation

--- src/main/java/de/dhbw/humbuch/view/SettingsView.java
+++ src/main/java/de/dhbw/humbuch/view/SettingsView.java
@@ -141,6 +141,7 @@ public class SettingsView extends VerticalLayout implements View,
 
 	public void init() {
 		tabs = new TabSheet();
+		tabs.setSizeFull();
 
 		buildUserTab();
 		buildCategoryTab();
@@ -148,6 +149,8 @@ public class SettingsView extends VerticalLayout implements View,
 
 		addListeners();
 		addComponent(tabs);
+		setMargin(true);
+		setSizeFull();
 	}
 
 	/**
@@ -156,7 +159,6 @@ public class SettingsView extends VerticalLayout implements View,
 	private void buildUserTab() {
 		tabUser.setMargin(true);
 		tabUser.setSpacing(true);
-		tabUser.setSizeFull();
 
 		tabUser.addComponent(userChangePw);
 
@@ -209,7 +211,7 @@ public class SettingsView extends VerticalLayout implements View,
 		changePwWindow.setModal(true);
 		// <----------------------------------------
 
-		tabs.addTab(tabUser, "User");
+		tabs.addTab(tabUser, "Benutzer");
 	}
 
 	/**
@@ -223,7 +225,22 @@ public class SettingsView extends VerticalLayout implements View,
 		tabCategories.setSpacing(true);
 		tabCategories.setSizeFull();
 
+		// Table
+		catTable.setSizeFull();
+		catTable.setSelectable(true);
+		catTable.setImmediate(true);
+		catTable.setBuffered(true);
+
+		catTable.setContainerDataSource(catData);
+		catTable.setVisibleColumns(new Object[] { CAT_NAME, CAT_DESCRIPTION });
+		catTable.setColumnHeader(CAT_NAME, "Kategorie");
+		catTable.setColumnHeader(CAT_DESCRIPTION, "Beschreibung");
+
+		tabCategories.addComponent(catTable);
+		tabCategories.setExpandRatio(catTable, 1);
+		
 		final HorizontalLayout catEditButtons = new HorizontalLayout();
+		catEditButtons.setSpacing(true);
 
 		catDelete.setEnabled(false);
 		catEdit.setEnabled(false);
@@ -236,29 +253,20 @@ public class SettingsView extends VerticalLayout implements View,
 		catEditButtons.addComponent(catSave);
 
 		final HorizontalLayout catAddButtons = new HorizontalLayout();
+		catAddButtons.setSpacing(true);
 		catAddButtons.addComponent(catAdd);
 		catAddButtons.addComponent(catDelete);
 
 		tabCategories.addComponent(new HorizontalLayout() {
 			{
 				setWidth("100%");
+				setHeight(null);
 				addComponent(catAddButtons);
 				addComponent(catEditButtons);
 				setComponentAlignment(catEditButtons, Alignment.MIDDLE_RIGHT);
 			}
 		});
-
-		// Table
-		catTable.setSizeFull();
-		catTable.setSelectable(true);
-		catTable.setImmediate(true);
-
-		catTable.setContainerDataSource(catData);
-		catTable.setVisibleColumns(new Object[] { CAT_NAME, CAT_DESCRIPTION });
-		catTable.setColumnHeader(CAT_NAME, "Kategorie");
-		catTable.setColumnHeader(CAT_DESCRIPTION, "Beschreibung");
-
-		tabCategories.addComponent(catTable);
+		
 		tabs.addTab(tabCategories, "Lehrmittelkategorien");
 
 		/*
@@ -281,7 +289,6 @@ public class SettingsView extends VerticalLayout implements View,
 			@Override
 			public void buttonClick(ClickEvent event) {
 				catConfigureEditable(true);
-
 			}
 		});
 
@@ -300,9 +307,13 @@ public class SettingsView extends VerticalLayout implements View,
 			@Override
 			public void buttonClick(ClickEvent event) {
 				catCommit();
-				catConfigureEditable(false);
+				catTable.commit();
 				Category item = (Category) catTable.getValue();
+				
+				System.out.println("Name: " + item.getName());
 				settingsViewModel.doUpdateCategory(item);
+				catConfigureEditable(false);
+				
 			}
 		});
 
@@ -313,7 +324,7 @@ public class SettingsView extends VerticalLayout implements View,
 			public void buttonClick(ClickEvent event) {
 				Category item = new Category.Builder("").description("")
 						.build();
-				catData.addBean(item);
+				catTable.addItem(item);
 				catTable.select(item);
 				catConfigureEditable(true);
 				catCancel.setVisible(false);
@@ -445,31 +456,6 @@ public class SettingsView extends VerticalLayout implements View,
 		tabDates.setMargin(true);
 		tabDates.setSpacing(true);
 
-		final HorizontalLayout yearEditButtons = new HorizontalLayout();
-
-		yearDelete.setEnabled(false);
-		yearEdit.setEnabled(false);
-		yearSave.setVisible(false);
-		yearSave.addStyleName("default");
-		yearCancel.setVisible(false);
-		
-		yearEditButtons.addComponent(yearEdit);
-		yearEditButtons.addComponent(yearCancel);
-		yearEditButtons.addComponent(yearSave);
-
-		final HorizontalLayout yearAddButtons = new HorizontalLayout();
-		yearAddButtons.addComponent(yearAdd);
-		yearAddButtons.addComponent(yearDelete);
-
-		tabDates.addComponent(new HorizontalLayout() {
-			{
-				setWidth("100%");
-				addComponent(yearAddButtons);
-				addComponent(yearEditButtons);
-				setComponentAlignment(yearEditButtons, Alignment.MIDDLE_RIGHT);
-			}
-		});
-
 		yearTable = new Table() {
 			private static final long serialVersionUID = 1L;
 
@@ -498,18 +484,48 @@ public class SettingsView extends VerticalLayout implements View,
 
 		yearTable.setVisibleColumns(new Object[] { YEAR_YEAR, YEAR_FROM,
 				YEAR_TO, YEAR_END_FIRST, YEAR_BEGIN_SEC });
-		yearTable.setColumnHeader(YEAR_YEAR, "Jahr");
+		yearTable.setColumnHeader(YEAR_YEAR, "Schuljahr");
 		yearTable.setColumnHeader(YEAR_FROM, "Beginn");
 		yearTable.setColumnHeader(YEAR_TO, "Ende");
 		yearTable.setColumnHeader(YEAR_END_FIRST, "Ende 1. Halbjahr");
 		yearTable.setColumnHeader(YEAR_BEGIN_SEC, "Begin 2. Halbjahr");
-		
+
 		yearTable.setColumnAlignment(YEAR_FROM, Table.Align.RIGHT);
 		yearTable.setColumnAlignment(YEAR_TO, Table.Align.RIGHT);
 		yearTable.setColumnAlignment(YEAR_END_FIRST, Table.Align.RIGHT);
 		yearTable.setColumnAlignment(YEAR_BEGIN_SEC, Table.Align.RIGHT);
 
 		tabDates.addComponent(yearTable);
+		tabDates.setExpandRatio(yearTable, 1);
+		
+		final HorizontalLayout yearEditButtons = new HorizontalLayout();
+		yearEditButtons.setSpacing(true);
+		
+		yearDelete.setEnabled(false);
+		yearEdit.setEnabled(false);
+		yearSave.setVisible(false);
+		yearSave.addStyleName("default");
+		yearCancel.setVisible(false);
+
+		yearEditButtons.addComponent(yearEdit);
+		yearEditButtons.addComponent(yearCancel);
+		yearEditButtons.addComponent(yearSave);
+
+		final HorizontalLayout yearAddButtons = new HorizontalLayout();
+		yearAddButtons.setSpacing(true);
+		yearAddButtons.addComponent(yearAdd);
+		yearAddButtons.addComponent(yearDelete);
+
+		tabDates.addComponent(new HorizontalLayout() {
+			{
+				setWidth("100%");
+				setHeight(null);
+				addComponent(yearAddButtons);
+				addComponent(yearEditButtons);
+				setComponentAlignment(yearEditButtons, Alignment.MIDDLE_RIGHT);
+			}
+		});
+		
 		tabs.addTab(tabDates, "Schuljahresdaten");
 
 		/*
@@ -553,6 +569,7 @@ public class SettingsView extends VerticalLayout implements View,
 				yearCommit();
 				yearConfigureEditable(false);
 				SchoolYear item = (SchoolYear) yearTable.getValue();
+				System.out.println(item.getYear());
 				settingsViewModel.doUpdateSchoolYear(item);
 			}
 		});
--- src/main/java/de/dhbw/humbuch/viewmodel/SettingsViewModel.java
+++ src/main/java/de/dhbw/humbuch/viewmodel/SettingsViewModel.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 
 import org.hibernate.criterion.Restrictions;
 
+import com.google.common.eventbus.EventBus;
 import com.google.inject.Inject;
 
 import de.davherrmann.mvvm.ActionHandler;
@@ -13,48 +14,71 @@ import de.davherrmann.mvvm.StateChangeListener;
 import de.davherrmann.mvvm.annotations.AfterVMBinding;
 import de.davherrmann.mvvm.annotations.HandlesAction;
 import de.davherrmann.mvvm.annotations.ProvidesState;
+import de.dhbw.humbuch.event.MessageEvent;
+import de.dhbw.humbuch.event.MessageEvent.Type;
 import de.dhbw.humbuch.model.DAO;
 import de.dhbw.humbuch.model.entity.Category;
 import de.dhbw.humbuch.model.entity.SchoolYear;
 import de.dhbw.humbuch.model.entity.User;
 
 public class SettingsViewModel {
-	
-	public interface DoUpdateUser extends ActionHandler {}
-	public interface DoPasswordChange extends ActionHandler {}
-	
-	public interface SchoolYears extends State<Collection<SchoolYear>> {}
-	public interface Categories extends State<Collection<Category>> {}
-	public interface PasswordChangeStatus extends State<ChangeStatus> {}
-	public interface NameChangeStatus extends State<ChangeStatus> {}
-	public interface UserName extends State<String> {}
-	public interface UserEmail extends State<String> {}
+
+	public interface DoUpdateUser extends ActionHandler {
+	}
+
+	public interface DoPasswordChange extends ActionHandler {
+	}
+
+	public interface SchoolYears extends State<Collection<SchoolYear>> {
+	}
+
+	public interface Categories extends State<Collection<Category>> {
+	}
+
+	public interface PasswordChangeStatus extends State<ChangeStatus> {
+	}
+
+	public interface NameChangeStatus extends State<ChangeStatus> {
+	}
+
+	public interface UserName extends State<String> {
+	}
+
+	public interface UserEmail extends State<String> {
+	}
 
 	@ProvidesState(SchoolYears.class)
-	private State<Collection<SchoolYear>> schoolYears = new BasicState<>(Collection.class);
-	
+	private State<Collection<SchoolYear>> schoolYears = new BasicState<>(
+			Collection.class);
+
 	@ProvidesState(Categories.class)
-	private State<Collection<Category>> categories = new BasicState<>(Collection.class);
-	
+	private State<Collection<Category>> categories = new BasicState<>(
+			Collection.class);
+
 	@ProvidesState(PasswordChangeStatus.class)
-	private State<ChangeStatus> passwordChangeStatus = new BasicState<>(ChangeStatus.class);
-	
+	private State<ChangeStatus> passwordChangeStatus = new BasicState<>(
+			ChangeStatus.class);
+
 	@ProvidesState(NameChangeStatus.class)
-	private State<ChangeStatus> nameChangeStatus = new BasicState<>(ChangeStatus.class);
-	
+	private State<ChangeStatus> nameChangeStatus = new BasicState<>(
+			ChangeStatus.class);
+
 	@ProvidesState(UserName.class)
 	private State<String> userName = new BasicState<>(String.class);
-	
+
 	@ProvidesState(UserEmail.class)
 	private State<String> userEmail = new BasicState<>(String.class);
-	
+
+	private EventBus eventBus;
 	private State<User> currentUser;
 	private DAO<SchoolYear> daoSchoolYear;
 	private DAO<User> daoUser;
 	private DAO<Category> daoCategory;
-	
+
 	@Inject
-	public SettingsViewModel(DAO<SchoolYear> daoSchoolYear, DAO<User> daoUser, DAO<Category> daoCategory, Properties properties) {
+	public SettingsViewModel(DAO<SchoolYear> daoSchoolYear, DAO<User> daoUser,
+			DAO<Category> daoCategory, Properties properties, EventBus eventBus) {
+		this.eventBus = eventBus;
 		this.daoSchoolYear = daoSchoolYear;
 		this.daoUser = daoUser;
 		this.daoCategory = daoCategory;
@@ -62,64 +86,73 @@ public class SettingsViewModel {
 		this.currentUser.addStateChangeListener(new StateChangeListener() {
 			@Override
 			public void stateChange(Object value) {
-				if(value != null)
+				if (value != null)
 					updateUser();
 			}
 		});
 	}
-	
+
 	@AfterVMBinding
 	private void afterAMBindung() {
 		updateSchoolYears();
 		updateCategories();
 		updateUser();
 	}
-	
+
 	private void updateSchoolYears() {
 		schoolYears.set(daoSchoolYear.findAll());
 	}
-	
+
 	private void updateCategories() {
 		categories.set(daoCategory.findAll());
 	}
-	
+
 	private void updateUser() {
 		userName.set(currentUser.get().getUsername());
 		userEmail.set(currentUser.get().getEmail());
 	}
-	
+
 	public void doUpdateSchoolYear(SchoolYear schoolYear) {
-		daoSchoolYear.update(schoolYear);
+		if (schoolYear.getYear().isEmpty()) {
+			eventBus.post(new MessageEvent("Speichern nicht möglich!",
+					"Das Feld 'Schuljahr' darf nicht leer sein.", Type.WARNING));
+			updateSchoolYears();
+		} else {
+			daoSchoolYear.update(schoolYear);
+		}
 	}
-	
+
 	public void doDeleteSchoolYear(SchoolYear schoolYear) {
 		daoSchoolYear.delete(schoolYear);
 		updateSchoolYears();
 	}
-	
+
 	public void doUpdateCategory(Category category) {
-		if (category.getName().isEmpty()  || category.getDescription().isEmpty()) {
-			// TODO: error Message: empty category!!
-			return;
+		if (category.getName().isEmpty()) {
+			eventBus.post(new MessageEvent("Speichern nicht möglich!",
+					"Das Feld 'Kategorie' darf nicht leer sein.", Type.WARNING));
+			
+		} else {
+			daoCategory.update(category);
 		}
-		daoCategory.update(category);
+		
+		//updateCategories();
 	}
-	
+
 	public void doDeleteCategory(Category category) {
 		daoCategory.delete(category);
 		updateCategories();
 	}
-	
+
 	@HandlesAction(DoUpdateUser.class)
 	public void doUpdateUser(String userName, String userEmail) {
-		if(userEmail.isEmpty())
+		if (userEmail.isEmpty())
 			userEmail = null;
-		Collection<User> userWithSameNameOrPassword = daoUser.findAllWithCriteria(
-				Restrictions.or(
+		Collection<User> userWithSameNameOrPassword = daoUser
+				.findAllWithCriteria(Restrictions.or(
 						Restrictions.eq("username", userName),
-						Restrictions.eq("email", userEmail)
-				));
-		if(!userWithSameNameOrPassword.isEmpty()) {
+						Restrictions.eq("email", userEmail)));
+		if (!userWithSameNameOrPassword.isEmpty()) {
 			User user = currentUser.get();
 			user.setUsername(userName);
 			user.setEmail(userEmail);
@@ -132,15 +165,17 @@ public class SettingsViewModel {
 			nameChangeStatus.set(ChangeStatus.NAME_OR_MAIL_ALREADY_EXISTS);
 		}
 	}
-	
-	@HandlesAction(DoPasswordChange.class) 
-	public void doPasswordChange(String currentPassword, String newPassword, String newPasswordVerified) {
+
+	@HandlesAction(DoPasswordChange.class)
+	public void doPasswordChange(String currentPassword, String newPassword,
+			String newPasswordVerified) {
 		User user = currentUser.get();
-		if(currentPassword.isEmpty() || newPassword.isEmpty() || newPasswordVerified.isEmpty()) {
+		if (currentPassword.isEmpty() || newPassword.isEmpty()
+				|| newPasswordVerified.isEmpty()) {
 			passwordChangeStatus.set(ChangeStatus.EMPTY_FIELDS);
-		} else if(!user.getPassword().equals(currentPassword)) {
+		} else if (!user.getPassword().equals(currentPassword)) {
 			passwordChangeStatus.set(ChangeStatus.CURRENT_PASSWORD_WRONG);
-		} else if(!newPassword.equals(newPasswordVerified)) {
+		} else if (!newPassword.equals(newPasswordVerified)) {
 			passwordChangeStatus.set(ChangeStatus.NEW_PASSWORD_NOT_EQUALS);
 		} else {
 			user.setPassword(newPassword);
@@ -149,13 +184,8 @@ public class SettingsViewModel {
 			passwordChangeStatus.set(ChangeStatus.SUCCESSFULL);
 		}
 	}
-	
+
 	public enum ChangeStatus {
-		EMPTY_FIELDS,
-		CURRENT_PASSWORD_WRONG,
-		NEW_PASSWORD_NOT_EQUALS,
-		NAME_OR_MAIL_ALREADY_EXISTS,
-		FAILED,
-		SUCCESSFULL;
+		EMPTY_FIELDS, CURRENT_PASSWORD_WRONG, NEW_PASSWORD_NOT_EQUALS, NAME_OR_MAIL_ALREADY_EXISTS, FAILED, SUCCESSFULL;
 	}
- }
+}
