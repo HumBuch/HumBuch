3123199639a0910ad20c5fb8f29b52585bb00f38
David Herrmann <davherrmann@googlemail.com>
2014-05-13 11:38:10 +0200

    added helper methods for creating schoolyears

--- src/test/java/de/dhbw/humbuch/test/TestUtils.java
+++ src/test/java/de/dhbw/humbuch/test/TestUtils.java
@@ -5,6 +5,7 @@ import java.util.Date;
 
 import de.dhbw.humbuch.model.entity.BorrowedMaterial;
 import de.dhbw.humbuch.model.entity.Grade;
+import de.dhbw.humbuch.model.entity.SchoolYear;
 import de.dhbw.humbuch.model.entity.SchoolYear.Term;
 import de.dhbw.humbuch.model.entity.Student;
 import de.dhbw.humbuch.model.entity.TeachingMaterial;
@@ -20,9 +21,39 @@ public class TestUtils {
 		calendar.add(Calendar.DATE, days);
 		return calendar.getTime();
 	}
+	
+	public static Grade grade(int grade) {
+		return new Grade.Builder(grade, "").build();
+	}
+	
+	public static SchoolYear schoolYear(int fromDays, int endFirstTermDays,
+			int beginSecondTermDays, int toDays) {
+		SchoolYear schoolYear = new SchoolYear.Builder("now",
+				todayPlusDays(fromDays), todayPlusDays(toDays))
+				.endFirstTerm(todayPlusDays(endFirstTermDays))
+				.beginSecondTerm(todayPlusDays(beginSecondTermDays)).build();
+		return schoolYear;
+	}
+
+	public static SchoolYear schoolYearFirstTermStarted() {
+		return schoolYear(-1, 1, 1, 2);
+	}
+
+	public static SchoolYear schoolYearFirstTermEnded() {
+		return schoolYear(-2, -1, 1, 2);
+	}
+
+	public static SchoolYear schoolYearSecondTermStarted() {
+		return schoolYear(-3, -2, -1, 1);
+	}
+
+	public static SchoolYear schoolYearSecondTermEnded() {
+		return schoolYear(-9, -8, -7, -6);
+	}
 
 	public static Student studentInGrade(int grade) {
 		Grade gradeEntity = new Grade.Builder(grade, "").build();
+		gradeEntity.setId(grade);
 		Student student = new Student.Builder(random(), "John", "Doe", null,
 				gradeEntity).build();
 		return student;
