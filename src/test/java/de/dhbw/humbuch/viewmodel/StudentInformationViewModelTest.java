5d832e6e3349fd6266f4c3951c728d7c92a85632
BennyRa <bennyra92@gmail.com>
2014-05-13 13:02:03 +0200

    Started to implement StudentInformationViewModelTest

--- src/test/java/de/dhbw/humbuch/viewmodel/StudentInformationViewModelTest.java
+++ src/test/java/de/dhbw/humbuch/viewmodel/StudentInformationViewModelTest.java
@@ -4,11 +4,24 @@ package de.dhbw.humbuch.viewmodel;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.EntityManager;
 
+import org.jfree.util.Log;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import au.com.bytecode.opencsv.CSVReader;
 
 import com.google.inject.Inject;
 import com.google.inject.Provider;
@@ -16,21 +29,40 @@ import com.google.inject.Provider;
 import de.dhbw.humbuch.guice.GuiceJUnitRunner;
 import de.dhbw.humbuch.guice.TestModule;
 import de.dhbw.humbuch.guice.GuiceJUnitRunner.GuiceModules;
+import de.dhbw.humbuch.model.DAO;
+import de.dhbw.humbuch.model.entity.Grade;
+import de.dhbw.humbuch.model.entity.Student;
 import de.dhbw.humbuch.model.entity.TestPersistenceInitialiser;
+import de.dhbw.humbuch.util.CSVHandler;
 
 @RunWith(GuiceJUnitRunner.class)
 @GuiceModules({ TestModule.class })
 public class StudentInformationViewModelTest extends BaseTest {
 
 	private StudentInformationViewModel vm;
+	private DAO<Student> daoStudent;
+	
+	private final static Logger LOG = LoggerFactory.getLogger(StudentInformationViewModelTest.class);
 
 	@Inject
 	public void setInjected(TestPersistenceInitialiser persistenceInitialiser,
 			Provider<EntityManager> emProvider,
-			StudentInformationViewModel studentInformationViewModel) {
+			StudentInformationViewModel studentInformationViewModel,
+			DAO<Student> daoStudent) {
+		this.daoStudent = daoStudent;
 		super.setInjected(persistenceInitialiser, emProvider);
 		
 		this.vm = studentInformationViewModel;
+	}
+	
+	private void importTwoStudents(boolean fullImport) {
+		Grade grade = new Grade.Builder(5, "").build();
+		List<Student> students = new ArrayList<Student>();
+		Student student1 = new Student.Builder(2, "Peter", "Doe", null, grade).build();
+		Student student2 = new Student.Builder(2, "Claude", "Gable", null, grade).build();
+		students.add(student1);
+		students.add(student2);		
+		vm.persistStudents(students, fullImport);
 	}
 	
 	@Before
@@ -41,5 +73,15 @@ public class StudentInformationViewModelTest extends BaseTest {
 	@Test
 	public void testStateInitialisation() {
 		assertNotNull(vm.students.get());
+	}
+	
+	@Test
+	public void testFullImport(){
+		LOG.info(""+daoStudent.findAll().size());
+		System.out.println(""+daoStudent.findAll().size());
+		importTwoStudents(true);
+		refreshViewModel();
+		assertEquals(2, daoStudent.findAll().size());
+		assertEquals("Claude", daoStudent.find(1).getFirstname());
 	}
 }
