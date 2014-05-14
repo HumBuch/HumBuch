ede22d78dcd2dffe81e68f814a87598b62664472
David Herrmann <davherrmann@googlemail.com>
2014-05-14 13:50:43 +0200

    use BaseTest

--- src/test/java/de/dhbw/humbuch/view/SettingsViewTest.java
+++ src/test/java/de/dhbw/humbuch/view/SettingsViewTest.java
@@ -1,12 +1,7 @@
 package de.dhbw.humbuch.view;
 
-import static de.dhbw.humbuch.test.TestUtils.randomUser;
-import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
-
 import java.util.ArrayList;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -14,8 +9,8 @@ import com.google.inject.Inject;
 import com.vaadin.ui.Field;
 
 import de.dhbw.humbuch.guice.GuiceJUnitRunner;
-import de.dhbw.humbuch.guice.TestModule;
 import de.dhbw.humbuch.guice.GuiceJUnitRunner.GuiceModules;
+import de.dhbw.humbuch.guice.TestModule;
 import de.dhbw.humbuch.model.DAO;
 import de.dhbw.humbuch.model.entity.TestPersistenceInitialiser;
 import de.dhbw.humbuch.model.entity.User;
@@ -23,42 +18,18 @@ import de.dhbw.humbuch.viewmodel.Properties;
 
 @RunWith(GuiceJUnitRunner.class)
 @GuiceModules({ TestModule.class })
-public class SettingsViewTest {
+public class SettingsViewTest extends BaseTest {
 
 	private SettingsView settingsView;
-	private Properties properties;
-	private DAO<User> daoUser;
 
 	@Inject
 	public void setInjected(MVVMConfig mvvmConfig,
 			TestPersistenceInitialiser testPersistenceInitialiser,
-			SettingsView settingsView,
+			SettingsView view,
 			Properties properties,
 			DAO<User> daoUser) {
-		this.settingsView = settingsView;
-		this.properties = properties;
-		this.daoUser = daoUser;
-
-	}
-	
-	@Before
-	public void setLoggedInUser() {
-		properties.currentUser.set(daoUser.insert(randomUser()));
-	}
-
-	@Test
-	public void testInstantiation() {
-		assertThat(settingsView, notNullValue());
-	}
-	
-	@Test
-	public void testEnter() {
-		settingsView.enter(null);
-	}
-	
-	@Test
-	public void testTitleNotNull() {
-		assertThat(settingsView.getTitle(), notNullValue());
+		this.settingsView = view;
+		super.setInjected(mvvmConfig, testPersistenceInitialiser, view, properties, daoUser);
 	}
 	
 	@Test
