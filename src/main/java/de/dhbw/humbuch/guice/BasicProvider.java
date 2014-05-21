9a71fc406f67883a05e93c1619c7f9f2f7ec81ce
David Herrmann <davherrmann@googlemail.com>
2014-05-21 09:34:29 +0200

    add javadoc

--- src/main/java/de/dhbw/humbuch/guice/BasicProvider.java
+++ src/main/java/de/dhbw/humbuch/guice/BasicProvider.java
@@ -10,6 +10,11 @@ import com.vaadin.ui.UI;
 import de.davherrmann.guice.vaadin.ScopedUIProvider;
 import de.davherrmann.guice.vaadin.UIKeyProvider;
 
+/**
+ * @author davherrmann
+ *
+ * {@link Provider} for providing scoped {@link UI}s
+ */
 public class BasicProvider extends ScopedUIProvider {
 	private static final long serialVersionUID = -8975200838602301472L;
 	
